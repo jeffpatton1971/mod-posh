d426c72f991185f269d06d3bb93f3e6b27236ad9
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-12 20:06:11 +0000

    A powershell template script, that incorporates some of the same things I do in my vbscripts.
    git-svn-id: https://mod-posh.googlecode.com/svn@82 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/template.ps1
@@ -0,0 +1,35 @@
+#
+#	Template Script
+#
+#	This script sets up the basic framework that I use
+#	for all my scripts.
+#
+#	$ScriptName is used to register events for this script
+#	in the $LogName log.
+#
+#	$LogName is which classic log you want to log to
+#		Application
+#		System
+#		Security
+#
+$ScriptName = ""
+$LogName = "Application"
+$ScriptPath = $MyInvocation.MyCommand.Path
+$Username = $env:USERDOMAIN + "\" + $env:USERNAME
+
+	New-EventLog -Source $ScriptName -LogName $LogName
+	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username
+	
+	LogEvent($ScriptName, $LogName, "100", "Information", $Message + "Started: " + (Get-Date).toString())
+	Main
+	LogEvent($ScriptName, $LogName, "100", "Information", $Message + "Finished: " + (Get-Date).toString())
+	
+Function Main()
+	{
+		#	This function kicks everything off.
+	}
+
+Function LogEvent($ScriptName, $LogName, $EventID, $EventType, $Message)
+	{
+		Write-EventLog -LogName $LogName -Source $ScriptName -EventID $EventID -EntryType $EntryType -Message $Message
+	}
