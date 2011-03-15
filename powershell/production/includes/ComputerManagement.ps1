26d557b18286762b6fb42e547cc32f0119c67293
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-15 15:25:50 +0000

    Added PowerShell V2 help code.
    git-svn-id: https://mod-posh.googlecode.com/svn@92 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ComputerManagement.ps1
+++ powershell/production/includes/ComputerManagement.ps1
@@ -13,6 +13,17 @@
 #
 Function Add-User($Computer, $User, $Password, $Description)
 	{
+	<#
+		.SYNOPSIS
+		Add a user account to the local computer.
+		.DESCRIPTION
+		This function will add a user account to the local computer. You will need
+		to run this with either UAC disabled or from an elevated prompt.
+		.EXAMPLE
+		add-user MyComputer MyUserAccount MyP@ssw0rd "This is my account."
+		.LINK
+		http://scripts.patton-tech.com/
+	#>
 		$objComputer = [ADSI]"WinNT://$Computer"
 		$objUser = $objComputer.Create("User", $User)
 		$objUser.setpassword($password)
@@ -23,12 +34,32 @@ Function Add-User($Computer, $User, $Password, $Description)
 
 Function Set-Pass($Computer, $User, $Password)
 	{
+	<#
+		.SYNOPSIS
+		Change the password of an existing user account.
+		.DESCRIPTION
+		This function will change the password for an existing user account. You will need
+		to run this with either UAC disabled or from an elevated prompt.
+		.EXAMPLE
+		set-pass MyComputer MyUserAccount N3wP@ssw0rd
+		.LINK
+		http://scripts.patton-tech.com/		
+	#>
 		$objUser=[adsi]("WinNT://$strComputer/$User, user")
 		$objUser.psbase.invoke("SetPassword", $Password)
 	}
 	
 Function Set-Group($Computer, $User, $Group)
 	{
+	<#
+		.SYNOPSIS
+		Add an existing user to a local group.
+		.DESCRIPTION
+		This function will add an existing user to an existing group. You will need
+		to run this with either UAC disabled or from an elevated prompt.
+		.LINK
+		http://scripts.patton-tech.com/		
+	#>
 		$objComputer = [ADSI]"WinNT://$Computer/$Group,group"
 		$objComputer.add("WinNT://$Computer/$User")
 	}
