fdf30b5c34a4fbf5cd9bc71068f097f2a617aef3
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-15 20:29:58 +0000

    Fleshed out the help a bit more, added parameters and updated the link.
    git-svn-id: https://mod-posh.googlecode.com/svn@95 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ActiveDirectoryManagement.ps1
+++ powershell/production/includes/ActiveDirectoryManagement.ps1
@@ -6,7 +6,7 @@
 #		$ADProperty is a list of properties you want returned
 #			and is accessible via .properties.propertyName
 #
-Function Get-ADObjects($objOU, $objectCategory, $ADProperty)
+Function Get-ADObjects
 	{
 	<#
 		.SYNOPSIS
@@ -15,13 +15,30 @@ Function Get-ADObjects($objOU, $objectCategory, $ADProperty)
 		This function will return a list of objects from ActiveDirectory. It will
 		start at the provided ADSPath and search for objectCategory. For each
 		objectCategory it finds it stores the ADProperty that was requested.
+		.PARAMETER objOU
+		This is the LDAP URI of the location within ActiveDirectory you would like to
+		search. This can be an OU, CN or even the root of your domain.
+		.PARAMETER objectCategory
+		This is the kind of object that you would like the search to return. Typical
+		values are; computer (default), user and group.
+		.PARAMETER ADProperty
+		If you want specific properties returned like name, or distinguishedName 
+		provide a comma seperated list.
 		.EXAMPLE
 		get-adobjects "LDAP://OU=Workstations,DC=company,DC=com" computer name
 		.EXAMPLE
 		get-adobjects "LDAP://CN=Users,DC=company,DC=com" user distinguishedName
 		.LINK
-		http://scripts.patton-tech.com/
+		http://scripts.patton-tech.com/wiki/PowerShell/ActiveDirectoryManagement
 	#>	
+		Param
+			(
+				[Parameter(Mandatory=$true)]
+				[string]$objOU,
+				[string]$objectCategory="computer",
+				[string]$ADProperty="name"
+			)
+		
 		if($objOU -eq $Null) 
 			{
 				$objOU = ""
