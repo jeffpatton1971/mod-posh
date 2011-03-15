94c1759b29853f14394185d6ebea0636f878cd23
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-15 22:05:54 +0000

    added a .NOTE section
    git-svn-id: https://mod-posh.googlecode.com/svn@100 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ComputerManagement.ps1
+++ powershell/production/includes/ComputerManagement.ps1
@@ -4,8 +4,7 @@ Function Add-User
 		.SYNOPSIS
 		Add a user account to the local computer.
 		.DESCRIPTION
-		This function will add a user account to the local computer. You will need
-		to run this with either UAC disabled or from an elevated prompt.
+		This function will add a user account to the local computer.
 		.PARAMETER Computer
 		The NetBIOS name of the computer that you will add the account to.
 		.PARAMETER User
@@ -15,6 +14,8 @@ Function Add-User
 		on the destination computer.
 		.PARAMETER Description
 		A description of what this account will be used for.
+		.NOTE
+		 You will need to run this with either UAC disabled or from an elevated prompt.
 		.EXAMPLE
 		add-user MyComputer MyUserAccount MyP@ssw0rd "This is my account."
 		.LINK
@@ -45,8 +46,7 @@ Function Set-Pass
 		.SYNOPSIS
 		Change the password of an existing user account.
 		.DESCRIPTION
-		This function will change the password for an existing user account. You will need
-		to run this with either UAC disabled or from an elevated prompt.
+		This function will change the password for an existing user account. 
 		.PARAMETER Computer
 		The NetBIOS name of the computer that you will add the account to.
 		.PARAMETER User
@@ -54,6 +54,8 @@ Function Set-Pass
 		.PARAMETER Password
 		The password for the account, this must follow password policies enforced
 		on the destination computer.
+		.NOTE
+		 You will need to run this with either UAC disabled or from an elevated prompt.
 		.EXAMPLE
 		set-pass MyComputer MyUserAccount N3wP@ssw0rd
 		.LINK
@@ -79,14 +81,15 @@ Function Set-Group
 		.SYNOPSIS
 		Add an existing user to a local group.
 		.DESCRIPTION
-		This function will add an existing user to an existing group. You will need
-		to run this with either UAC disabled or from an elevated prompt.
+		This function will add an existing user to an existing group.
 		.PARAMETER Computer
 		The NetBIOS name of the computer that you will add the account to.
 		.PARAMETER User
 		The user name of the account that will be created.
 		.PARAMETER Group
 		The name of an existing group to add this user to.
+		.NOTE
+		 You will need to run this with either UAC disabled or from an elevated prompt.
 		.EXAMPLE
 		set-group MyComputer MyUserAccount Administrators
 		.LINK
