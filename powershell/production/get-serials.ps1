0938be0da9de5b6e721e4c6de5ba53bd78da9d7d
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-15 04:14:32 +0000

    Adjusted script to account for the new template.ps1
    git-svn-id: https://mod-posh.googlecode.com/svn@90 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/get-serials.ps1
+++ powershell/production/get-serials.ps1
@@ -1,5 +1,5 @@
 #
-#	Template Script
+#	Get-Serials
 #
 #	This script sets up the basic framework that I use
 #	for all my scripts.
@@ -12,37 +12,29 @@
 #		System
 #		Security
 #
-$ScriptName = "get-serials"
+$ScriptName = $MyInvocation.MyCommand.ToString()
 $LogName = "Application"
 $ScriptPath = $MyInvocation.MyCommand.Path
 $Username = $env:USERDOMAIN + "\" + $env:USERNAME
 
-	New-EventLog -Source $ScriptName -LogName $LogName
-	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username
+	New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
 	
-	LogEvent $ScriptName $LogName "100" "Information" $Message + "Started: " + (Get-Date).toString()
-	Main
-	LogEvent $ScriptName $LogName "100" "Information" $Message + "Finished: " + (Get-Date).toString()
-	
-Function Main()
-	{
-		#	Dotsource in the AD functions I need
-		. .\includes\ActiveDirectoryManagement.ps1
-		$ADSPath =  Read-Host "Please provide an LDAP URL"
-		$computers = Get-ADObjects $ADSPath "computer" "name"
-		foreach ($computer in $computers)
-			{
-				if ($computer -eq $null){}
-				else
-					{
-						write-host $computer.Properties.name
-						$serial = Get-WmiObject -query "select SerialNumber from win32_bios" -computername $computer.Properties.name
-						write-host $serial.serialnumber
-					}
-			}
-	}
+	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
 
-Function LogEvent($ScriptName, $LogName, $EventID, $EventType, $Message)
-	{
-		Write-EventLog -LogName $LogName -Source $ScriptName -EventID $EventID -EntryType $EntryType -Message $Message
-	}
+	#	Dotsource in the AD functions I need
+	. .\includes\ActiveDirectoryManagement.ps1
+	$ADSPath =  Read-Host "Please provide an LDAP URL"
+	$computers = Get-ADObjects $ADSPath "computer" "name"
+	foreach ($computer in $computers)
+		{
+			if ($computer -eq $null){}
+			else
+				{
+					write-host $computer.Properties.name
+					$serial = Get-WmiObject -query "select SerialNumber from win32_bios" -computername $computer.Properties.name
+					write-host $serial.serialnumber
+				}
+		}
+	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message
