089e585e0103f5cdecada9b7a02a488638833307
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-16 19:47:15 +0000

    Adjusted white-space
    git-svn-id: https://mod-posh.googlecode.com/svn@103 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ActiveDirectoryManagement.ps1
+++ powershell/production/includes/ActiveDirectoryManagement.ps1
@@ -2,35 +2,35 @@ Function Get-ADObjects
 	{
 	<#
 		.SYNOPSIS
-		Returns a list of objects from ActiveDirectory
+			Returns a list of objects from ActiveDirectory
 		.DESCRIPTION
-		This function will return a list of objects from ActiveDirectory. It will
-		start at the provided ADSPath and search for objectCategory. For each
-		objectCategory it finds it stores the ADProperty that was requested.
+			This function will return a list of objects from ActiveDirectory. It will
+			start at the provided ADSPath and search for objectCategory. For each
+			objectCategory it finds it stores the ADProperty that was requested.
 		.PARAMETER objOU
-		This is the LDAP URI of the location within ActiveDirectory you would like to
-		search. This can be an OU, CN or even the root of your domain.
+			This is the LDAP URI of the location within ActiveDirectory you would like to
+			search. This can be an OU, CN or even the root of your domain.
 		.PARAMETER objectCategory
-		This is the kind of object that you would like the search to return. Typical
-		values are; computer (default), user and group.
+			This is the kind of object that you would like the search to return. Typical
+			values are; computer (default), user and group.
 		.PARAMETER ADProperty
-		If you want specific properties returned like name, or distinguishedName 
-		provide a comma seperated list.
+			If you want specific properties returned like name, or distinguishedName 
+			provide a comma seperated list.
 		.EXAMPLE
-		This exmaple returns a list of computers found in this OU
-		get-adobjects "LDAP://OU=Workstations,DC=company,DC=com"
+			This exmaple returns a list of computers found in this OU
+			get-adobjects "LDAP://OU=Workstations,DC=company,DC=com"
 		.EXAMPLE
-		This example returns a list of user in this container
-		get-adobjects "LDAP://CN=Users,DC=company,DC=com" user distinguishedName
+			This example returns a list of user in this container
+			get-adobjects "LDAP://CN=Users,DC=company,DC=com" user distinguishedName
 		.EXAMPLE
-		This example returns the objectSid of the named computer
-		get-adobjects "LDAP://CN=MyComputer,OU=Workstations,DC=company,DC=com" computer objectSid
+			This example returns the objectSid of the named computer
+			get-adobjects "LDAP://CN=MyComputer,OU=Workstations,DC=company,DC=com" computer objectSid
 		.NOTES
-		The script runs under the users context, so the user account must have permissions
-		to view the objects within the domain that the function is currently running
-		against.
+			The script runs under the users context, so the user account must have permissions
+			to view the objects within the domain that the function is currently running
+			against.
 		.LINK
-		http://scripts.patton-tech.com/wiki/PowerShell/ActiveDirectoryManagement
+			http://scripts.patton-tech.com/wiki/PowerShell/ActiveDirectoryManagement
 	#>	
 		Param
 			(
--- powershell/production/includes/ComputerManagement.ps1
+++ powershell/production/includes/ComputerManagement.ps1
@@ -2,24 +2,24 @@ Function Add-User
 	{
 	<#
 		.SYNOPSIS
-		Add a user account to the local computer.
+			Add a user account to the local computer.
 		.DESCRIPTION
-		This function will add a user account to the local computer.
+			This function will add a user account to the local computer.
 		.PARAMETER Computer
-		The NetBIOS name of the computer that you will add the account to.
+			The NetBIOS name of the computer that you will add the account to.
 		.PARAMETER User
-		The user name of the account that will be created.
+			The user name of the account that will be created.
 		.PARAMETER Password
-		The password for the account, this must follow password policies enforced
-		on the destination computer.
+			The password for the account, this must follow password policies enforced
+			on the destination computer.
 		.PARAMETER Description
-		A description of what this account will be used for.
+			A description of what this account will be used for.
 		.NOTE
-		 You will need to run this with either UAC disabled or from an elevated prompt.
+			You will need to run this with either UAC disabled or from an elevated prompt.
 		.EXAMPLE
-		add-user MyComputer MyUserAccount MyP@ssw0rd "This is my account."
+			add-user MyComputer MyUserAccount MyP@ssw0rd "This is my account."
 		.LINK
-		http://scripts.patton-tech.com/wiki/PowerShell/ComputerManagement
+			http://scripts.patton-tech.com/wiki/PowerShell/ComputerManagement
 	#>
 		Param
 			(
@@ -44,22 +44,22 @@ Function Set-Pass
 	{
 	<#
 		.SYNOPSIS
-		Change the password of an existing user account.
+			Change the password of an existing user account.
 		.DESCRIPTION
-		This function will change the password for an existing user account. 
+			This function will change the password for an existing user account. 
 		.PARAMETER Computer
-		The NetBIOS name of the computer that you will add the account to.
+			The NetBIOS name of the computer that you will add the account to.
 		.PARAMETER User
-		The user name of the account that will be created.
+			The user name of the account that will be created.
 		.PARAMETER Password
-		The password for the account, this must follow password policies enforced
-		on the destination computer.
+			The password for the account, this must follow password policies enforced
+			on the destination computer.
 		.NOTE
-		 You will need to run this with either UAC disabled or from an elevated prompt.
+			You will need to run this with either UAC disabled or from an elevated prompt.
 		.EXAMPLE
-		set-pass MyComputer MyUserAccount N3wP@ssw0rd
+			set-pass MyComputer MyUserAccount N3wP@ssw0rd
 		.LINK
-		http://scripts.patton-tech.com/wiki/PowerShell/ComputerManagement
+			http://scripts.patton-tech.com/wiki/PowerShell/ComputerManagement
 	#>
 		Param
 			(
@@ -79,21 +79,21 @@ Function Set-Group
 	{
 	<#
 		.SYNOPSIS
-		Add an existing user to a local group.
+			Add an existing user to a local group.
 		.DESCRIPTION
-		This function will add an existing user to an existing group.
+			This function will add an existing user to an existing group.
 		.PARAMETER Computer
-		The NetBIOS name of the computer that you will add the account to.
+			The NetBIOS name of the computer that you will add the account to.
 		.PARAMETER User
-		The user name of the account that will be created.
+			The user name of the account that will be created.
 		.PARAMETER Group
-		The name of an existing group to add this user to.
+			The name of an existing group to add this user to.
 		.NOTE
-		 You will need to run this with either UAC disabled or from an elevated prompt.
+			You will need to run this with either UAC disabled or from an elevated prompt.
 		.EXAMPLE
-		set-group MyComputer MyUserAccount Administrators
+			set-group MyComputer MyUserAccount Administrators
 		.LINK
-		http://scripts.patton-tech.com/wiki/PowerShell/ComputerManagement
+			http://scripts.patton-tech.com/wiki/PowerShell/ComputerManagement
 	#>
 		Param
 			(
