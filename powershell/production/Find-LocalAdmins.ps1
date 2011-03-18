5d236478c4dd41bfc7f90ff13d46b8d916fe82c8
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-18 04:29:58 +0000

    A script that reports the members of the Administrators group for each computer in the LDAP search path.
    git-svn-id: https://mod-posh.googlecode.com/svn@128 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/Find-LocalAdmins.ps1
@@ -0,0 +1,35 @@
+#
+#	Find Local Admins
+#
+#	This script searches ActiveDirectory for computers.
+#	It then queries each computer for the list of users
+#	who are in the local Administrators group.
+#
+$ScriptName = $MyInvocation.MyCommand.ToString()
+$LogName = "Application"
+$ScriptPath = $MyInvocation.MyCommand.Path
+$Username = $env:USERDOMAIN + "\" + $env:USERNAME
+
+	New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+	
+	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
+	
+	#	Dotsource in the functions you need.
+	. .\includes\ActiveDirectoryManagement.ps1
+	
+	$computers = Get-ADObjects "LDAP://OU=People,DC=soecs,DC=edu"
+	
+	foreach ($computer in $computers)
+		{
+			if ($computer -eq $null){}
+			else
+				{
+					write-host $computer.Properties.name
+					$groups = Get-LocalGroupMembers $computer.Properties.name Administrators
+					$groups | Format-Table
+				}
+		}
+	
+	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message	
