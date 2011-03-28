44d21d422090d935a2c4d9f79cb717a587587076
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-03-28 05:45:28 +0000

    Changed library to be more generic to work with files.
    git-svn-id: https://mod-posh.googlecode.com/svn@139 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/includes/FileManagement.ps1
@@ -0,0 +1,60 @@
+Function Update-Content
+	{
+		<#
+			.SYNOPSIS
+				Updates the contents of a file.
+			.DESCRIPTION
+				This function adds content to the top or bottom of an existing file.
+			.PARAMETER File
+				The full path and filename to the file to update
+			.PARAMETER Content
+				Content to add to the file
+			.PARAMETER Beginning
+				True or False to add content to top (true) or bottom (false) of file
+			.EXAMPLE
+				$Header = "Host Identity User DateTime Offset Request Status Size Referrer UserAgent"
+				$LogFile = C:\logfiles\access.log
+				
+				Update-Content $LogFile $Header
+				
+				This example sets a variable named header to be used in the command-line. We then pass the path and
+				header variable into the function for processing.
+			.LINK
+				http://scripts.patton-tech.com/wiki/PowerShell/ApacheHttpdManagement#Update-ContentTop
+		#>
+
+		Param
+			(
+				[Parameter(Mandatory=$true)]
+				[string]$File,
+				[Parameter(Mandatory=$true)]
+				[string]$Content,
+				[Parameter(Mandatory=$true)]
+				[bool]$Beginning=$true
+			)	
+
+		if (Test-Path $File -eq $True)
+			{
+				$Data = Get-Content $File
+				if ($Beginning -eq $True)
+					{
+						if ($Data[0].Contains($Content) -eq $True)
+							{
+								Write-Host "Skipping $File, content exists"
+							}
+						else
+							{
+								Write-Host "Writing content to $File"
+								Set-Content $File -Value $Content,$Data
+							}
+					}
+				else
+					{
+						Add-Content -Path $File -Value $Content
+					}
+			}
+		else
+			{
+				Write-Host "File $File does not exist"
+			}
+	}
