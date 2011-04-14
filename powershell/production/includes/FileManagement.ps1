99e664f8706e65da692119ee79a92d212d0d4b13
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-04-14 16:42:01 +0000

    I liked the New-LogFile functino so much from the scripting games entry i made, i decided to bring it over here.
    git-svn-id: https://mod-posh.googlecode.com/svn@183 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/FileManagement.ps1
+++ powershell/production/includes/FileManagement.ps1
@@ -55,3 +55,63 @@ Function Update-Content
 				Write-Host "File $ThisFile does not exist"
 			}
 	}
+Function New-LogFile
+    {
+        <#
+            .SYNOPSIS
+                Create a logfile
+            .DESCRIPTION
+                This function create's a log file in the provided folder, whose data can be piped in via the command
+                line or passed directly to the function. The filename for the log is generated from the current system
+                date, and if a file with same name exists, a new file is created with an incremented digit for each
+                matched filename.
+            .PARAMETER LogData
+                This is the information to send to the log.
+            .PARAMETER LogPath
+                This is the destination of the log file. If the provided folder doesn't exist on the computer, it 
+                is created automatically.
+            .EXAMPLE
+                Get-Process |New-LogFile
+                
+                Description
+                -----------
+                This example shows piping the output from Get-Process into the New-LogFile function, the default
+                log folder (C:\LogFiles) is used.
+            .EXAMPLE
+                Get-Process |New-LogFile -LogPath c:\logging
+                
+                Description
+                -----------
+                This example shows piping the output from Get-Process into the New-LogFile function and specifying a
+                different folder to store the logs in.
+            .EXAMPLE
+                New-LogFile -LogPath c:\logging -LogData "Script finished execution."
+                
+                Description
+                -----------
+                This example shows passing in the LogPath and LogData parameters to the New-LogFile function directly.
+            .NOTES
+                This space intentionally left blank.
+            .LINK
+                http://scripts.patton-tech.com/wiki/PowerShell/FileManagement#New-LogFile
+        #>
+        
+        Param
+            (
+                [Parameter(ValueFromPipeline=$true)]
+                $LogData,
+                $LogPath = "C:\LogFiles"
+            )
+
+        $FileName = (Get-Date).ToString('yyyyMMdd')
+        If ((Test-Path $LogPath) -ne $true)
+            {
+                New-Item $LogPath -ItemType Directory
+            }
+        If ((Test-Path (($LogPath + "\" + $FileName) + ".log")) -eq $true)
+            {
+                $FileName = (Get-Date).ToString('yyyyMMdd') + "-" `
+                            + @((Get-ChildItem $LogPath -Filter ($fileName + "*"))).count
+            }
+        $LogData |Out-File -FilePath (($LogPath + "\" + $FileName) + ".log") -Encoding ASCII -NoClobber
+    }
