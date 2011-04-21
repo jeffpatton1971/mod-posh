2aa03411870ea420220282f76b3075178fa6db4c
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-04-21 22:19:01 +0000

    Going to start adding Begin/Process/End to production scripts
    git-svn-id: https://mod-posh.googlecode.com/svn@221 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/get-serials.ps1
+++ powershell/production/get-serials.ps1
@@ -36,55 +36,60 @@ Param
         [Parameter(Mandatory=$true)]
         $ADSPath
     )
-    
-$ScriptName = $MyInvocation.MyCommand.ToString()
-$LogName = "Application"
-$ScriptPath = $MyInvocation.MyCommand.Path
-$Username = $env:USERDOMAIN + "\" + $env:USERNAME
+Begin
+    {    
+        $ScriptName = $MyInvocation.MyCommand.ToString()
+        $LogName = "Application"
+        $ScriptPath = $MyInvocation.MyCommand.Path
+        $Username = $env:USERDOMAIN + "\" + $env:USERNAME
 
-	New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
-	
-	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
-	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
-
-	#	Dotsource in the AD functions I need
-	. .\includes\ActiveDirectoryManagement.ps1
-
-	$Workstations = Get-ADObjects -ADSPath $ADSPath
-    
-    $Jobs = @()
-	foreach ($Workstation in $Workstations)
-		{
-            [string]$ThisWorkstation = $Workstation.Properties.name
-            $ThisJob = New-Object PSobject
-			if ($Workstation -eq $null){}
-			else
-				{
-                    Try
-                        {
-                            $ErrorActionPreference = "Stop"
-                            $Serial = Get-WmiObject -query "select SerialNumber from win32_bios" `
-                                    -computername $ThisWorkstation
-                            $Return = $serial.serialnumber
-                        }
-                    Catch
-                        {
-                            [string]$ThisError = $Error[0].Exception
-                            $ThisError = $ThisError.Substring($ThisError.IndexOf(":"))
-                            $ThisError = $ThisError.Substring(1,$ThisError.IndexOf("`r"))
-                            $return = $ThisError.Trim()
-                        }
-                    
-                    Add-Member -InputObject $ThisJob -MemberType NoteProperty -Name "ComputerName" `
-                        -Value $ThisWorkstation
-                    Add-Member -InputObject $ThisJob -MemberType NoteProperty -Name "ServiceTag" -Value $Return
-				}
-            $Jobs += $ThisJob
-            $ThisJob
-		}
-
-    $Message = [system.string]::Join("`n",($Jobs))
-    Write-EventLog -LogName $LogName -Source $ScriptName -EventId "101" -EntryType "Information" -Message $Message
+        New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+        	
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
+        #	Dotsource in the AD functions I need
+        . .\includes\ActiveDirectoryManagement.ps1
+    }
+Process
+    {
+    	$Workstations = Get-ADObjects -ADSPath $ADSPath
         
-	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
-	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message
+        $Jobs = @()
+    	foreach ($Workstation in $Workstations)
+    		{
+                [string]$ThisWorkstation = $Workstation.Properties.name
+                $ThisJob = New-Object PSobject
+    			if ($Workstation -eq $null){}
+    			else
+    				{
+                        Try
+                            {
+                                $ErrorActionPreference = "Stop"
+                                $Serial = Get-WmiObject -query "select SerialNumber from win32_bios" `
+                                        -computername $ThisWorkstation
+                                $Return = $serial.serialnumber
+                            }
+                        Catch
+                            {
+                                [string]$ThisError = $Error[0].Exception
+                                $ThisError = $ThisError.Substring($ThisError.IndexOf(":"))
+                                $ThisError = $ThisError.Substring(1,$ThisError.IndexOf("`r"))
+                                $return = $ThisError.Trim()
+                            }
+                        
+                        Add-Member -InputObject $ThisJob -MemberType NoteProperty -Name "ComputerName" `
+                            -Value $ThisWorkstation
+                        Add-Member -InputObject $ThisJob -MemberType NoteProperty -Name "ServiceTag" -Value $Return
+    				}
+                $Jobs += $ThisJob
+                $ThisJob
+    		}
+
+        $Message = [system.string]::Join("`n",($Jobs))
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventId "101" -EntryType "Information" -Message $Message
+    }
+End
+    {        
+    	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+    	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message
+    }
