9929c8503b9385af84d35098a11dff9fe125de66
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-04-21 21:53:54 +0000

    Updated get-serials.ps1 to accomodate the inline help i've been doing with my functions. added a try/catch to handle errors returned from get-wmiobject. in order to handle errors erroractionpreference had to be set to stop, since the error returned in the try was a non-stopping error. script now returns objects that can be piped to export-csv
    git-svn-id: https://mod-posh.googlecode.com/svn@219 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/get-serials.ps1
+++ powershell/production/get-serials.ps1
@@ -1,17 +1,23 @@
-#
-#	Get-Serials
-#
-#	This script sets up the basic framework that I use
-#	for all my scripts.
-#
-#	$ScriptName is used to register events for this script
-#	in the $LogName log.
-#
-#	$LogName is which classic log you want to log to
-#		Application
-#		System
-#		Security
-#
+<#
+    .SYNOPSIS
+        Get the BIOS serial numbers from computers in the domain.
+    .DESCRIPTION
+        Return a list of computers with their serial numbers. For Dell computers the Win32_BIOS.SerialNumber property
+        is the service tag of the computer. This identifies the computer on the Dell support site, and with it you can
+        get the proper drivers/manuals and warranty information.
+    .PARAMETER ADSPath
+        The location within Active Directory to find computers.
+    .EXAMPLE
+    .NOTES
+    .LINK
+#>
+
+Param
+    (
+        [Parameter(Mandatory=$true)]
+        $ADSPath
+    )
+    
 $ScriptName = $MyInvocation.MyCommand.ToString()
 $LogName = "Application"
 $ScriptPath = $MyInvocation.MyCommand.Path
@@ -25,16 +31,35 @@ $Username = $env:USERDOMAIN + "\" + $env:USERNAME
 	#	Dotsource in the AD functions I need
 	. .\includes\ActiveDirectoryManagement.ps1
 
-	$computers = Get-ADObjects $args[0] $args[1] $args[2]
-	foreach ($computer in $computers)
+	$Workstations = Get-ADObjects -ADSPath $ADSPath
+    
+    $Jobs = @()
+	foreach ($Workstation in $Workstations)
 		{
-			if ($computer -eq $null){}
+            [string]$ThisWorkstation = $Workstation.Properties.name
+            $ThisJob = New-Object PSobject
+			if ($Workstation -eq $null){}
 			else
 				{
-					write-host $computer.Properties.name
-					$serial = Get-WmiObject -query "select SerialNumber from win32_bios" -computername $computer.Properties.name
-					write-host $serial.serialnumber
+                    Try
+                        {
+                            $ErrorActionPreference = "Stop"
+                            $Serial = Get-WmiObject -query "select SerialNumber from win32_bios" -computername $ThisWorkstation
+                            $Return = $serial.serialnumber
+                        }
+                    Catch
+                        {
+                            [string]$ThisError = $Error[0].Exception
+                            $ThisError = $ThisError.Substring($ThisError.IndexOf(":"))
+                            $ThisError = $ThisError.Substring(1,$ThisError.IndexOf("`r"))
+                            $return = $ThisError.Trim()
+                        }
+                    
+                    Add-Member -InputObject $ThisJob -MemberType NoteProperty -Name "ComputerName" -Value $ThisWorkstation
+                    Add-Member -InputObject $ThisJob -MemberType NoteProperty -Name "ServiceTag" -Value $Return
 				}
+            $Jobs += $ThisJob
+            $ThisJob
 		}
 	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
 	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message
