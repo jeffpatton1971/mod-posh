b99a1d06cea682d24fd1e2cfe051250b34308bd0
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-04-21 22:30:09 +0000

    BPE
    git-svn-id: https://mod-posh.googlecode.com/svn@227 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/template.ps1
+++ powershell/production/template.ps1
@@ -6,22 +6,36 @@
     .PARAMETER
     .EXAMPLE
     .NOTES
+        ScriptName:
+        Created By: Jeff Patton
+        Date Coded:
         ScriptName is used to register events for this script
         LogName is used to determine which classic log to write to
     .LINK
 #>
+Param
+    (
+    
+    )
+Begin
+    {
+        $ScriptName = $MyInvocation.MyCommand.ToString()
+        $LogName = "Application"
+        $ScriptPath = $MyInvocation.MyCommand.Path
+        $Username = $env:USERDOMAIN + "\" + $env:USERNAME
 
-    $ScriptName = $MyInvocation.MyCommand.ToString()
-    $LogName = "Application"
-    $ScriptPath = $MyInvocation.MyCommand.Path
-    $Username = $env:USERDOMAIN + "\" + $env:USERNAME
+        New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
 
-	New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
-	
-	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
-	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
-	
-	#	Dotsource in the functions you need.
-	
-	$Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
-	Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message	
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
+
+        #	Dotsource in the functions you need.
+    }
+Process
+    {
+    }
+End
+    {
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message	
+    }
