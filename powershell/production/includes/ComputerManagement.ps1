a12e2afb5f312ab7d5d8e77d6cdb5d97a90511b4
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-04-28 22:46:13 +0000

    New function Get-PendingUpdates, this was pulled from a much larger script from Boe Prox, I stripped out much of what that script did since i'm using it as a function to return updates.
    http://boeprox.wordpress.com/2011/03/07/find-pending-updates-on-local-or-remote-computers/
    
    git-svn-id: https://mod-posh.googlecode.com/svn@250 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ComputerManagement.ps1
+++ powershell/production/includes/ComputerManagement.ps1
@@ -558,3 +558,57 @@ Function Get-LocalUserAccounts
 
         Return Invoke-Expression $ScriptBlock |Select-Object Name, SID
     }
+Function Get-PendingUpdates
+    {
+        <#
+            .SYNOPSIS
+                Retrieves the updates waiting to be installed from WSUS
+            .DESCRIPTION
+                Retrieves the updates waiting to be installed from WSUS
+            .PARAMETER ComputerName
+                Computer or computers to find updates for.
+            .EXAMPLE
+                Get-PendingUpdates 
+                Description
+                -----------
+                Retrieves the updates that are available to install on the local system
+            .NOTES
+                Author: Boe Prox
+                Date Created: 05Mar2011
+                RPC Dynamic Ports need to be enabled on inbound remote servers.
+        #> 
+
+        Param
+            (
+                [Parameter(ValueFromPipeline = $True)]
+                [string]$ComputerName
+            )
+        
+        Begin 
+            {
+            }
+        Process 
+            {
+                ForEach ($Computer in $ComputerName) 
+                    {
+                        If (Test-Connection -ComputerName $Computer -Count 1 -Quiet) 
+                            {
+                                Try 
+                                {
+                                    $Updates =  [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session",$Computer))
+                                    $Searcher = $Updates.CreateUpdateSearcher() 
+                                    $searchresult = $Searcher.Search("IsInstalled=0")     
+                                }
+                                Catch 
+                                {
+                                Write-Warning "$($Error[0])"
+                                Break
+                                } 
+                            }
+                    }
+            }
+        End 
+            {
+                Return $SearchResult.Updates
+            }
+    }
