d339ba09844c148461de0aa814efd56246dcf57e
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-04-28 19:31:27 +0000

    checking in the NetworkManagement.ps1 library with the new get-netstatreport function pulled from netstat.ps1 at the playground.
    git-svn-id: https://mod-posh.googlecode.com/svn@244 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/includes/NetworkManagement.ps1
@@ -0,0 +1,101 @@
+Function Get-HostName
+	{
+		<#
+			.SYNOPSIS
+				Returns the hostname from the provided IP
+			.DESCRIPTION
+				Reutrns an object that contains the hostname from a DNS server query
+			.PARAMETER Computer
+				The IP Address of a computer
+			.EXAMPLE
+			.NOTES
+			.LINK
+		#>
+		
+		Param
+			(
+				[Parameter(Mandatory=$true)]
+				[string]$Computer
+				
+		[System.Net.Dns]::GetHostEntry($Computer)
+	}
+Function Get-HostIp
+	{
+		<#
+			.SYNOPSIS
+			.DESCRIPTION
+			.PARAMETER
+			.EXAMPLE
+			.NOTES
+			.LINK
+		#>
+		
+		Param
+			(
+				[Parameter(Mandatory=$true)]
+				[string]$Computer
+				
+		[System.Net.Dns]::GetHostAddresses($Computer)
+	}
+Function Get-NetstatReport
+    {
+        <#
+            .SYNOPSIS
+            .DESCRIPTION
+            .EXAMPLE
+            .NOTES
+            .LINK
+        #>
+        $netstat = netstat -a -n -o | where-object { $_ -match "(UDP|TCP)" }
+        [regex]$regexTCP = '(?<Protocol>\S+)\s+((?<LAddress>(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?))|(?<LAddress>\[?[0-9a-fA-f]{0,4}(\:([0-9a-fA-f]{0,4})){1,7}\%?\d?\]))\:(?<Lport>\d+)\s+((?<Raddress>(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?))|(?<RAddress>\[?[0-9a-fA-f]{0,4}(\:([0-9a-fA-f]{0,4})){1,7}\%?\d?\]))\:(?<RPort>\d+)\s+(?<State>\w+)\s+(?<PID>\d+$)'
+
+        [regex]$regexUDP = '(?<Protocol>\S+)\s+((?<LAddress>(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?))|(?<LAddress>\[?[0-9a-fA-f]{0,4}(\:([0-9a-fA-f]{0,4})){1,7}\%?\d?\]))\:(?<Lport>\d+)\s+(?<RAddress>\*)\:(?<RPort>\*)\s+(?<PID>\d+)'
+        $Report = @()
+
+        foreach ($Line in $Netstat)
+        {
+            switch -regex ($Line.Trim())
+            {
+                $RegexTCP
+                {
+                    $MyProtocol = $Matches.Protocol
+                    $MyLocalAddress = $Matches.LAddress
+                    $MyLocalPort = $Matches.LPort
+                    $MyRemoteAddress = $Matches.Raddress
+                    $MyRemotePort = $Matches.RPort
+                    $MyState = $Matches.State
+                    $MyPID = $Matches.PID
+                    $MyProcessName = (Get-Process -Id $Matches.PID -ErrorAction SilentlyContinue).ProcessName
+                    $MyProcessPath = (Get-Process -Id $Matches.PID -ErrorAction SilentlyContinue).Path
+                    $MyUser = (Get-WmiObject -Class Win32_Process -Filter ("ProcessId = "+$Matches.PID)).GetOwner().User
+                }
+                $RegexUDP
+                {
+                    $MyProtocol = $Matches.Protocol
+                    $MyLocalAddress = $Matches.LAddress
+                    $MyLocalPort = $Matches.LPort
+                    $MyRemoteAddress = $Matches.Raddress
+                    $MyRemotePort = $Matches.RPort
+                    $MyState = $Matches.State
+                    $MyPID = $Matches.PID
+                    $MyProcessName = (Get-Process -Id $Matches.PID -ErrorAction SilentlyContinue).ProcessName
+                    $MyProcessPath = (Get-Process -Id $Matches.PID -ErrorAction SilentlyContinue).Path
+                    $MyUser = (Get-WmiObject -Class Win32_Process -Filter ("ProcessId = "+$Matches.PID)).GetOwner().User
+                }
+            }
+            $LineItem = New-Object -TypeName PSobject -Property @{
+                Protocol = $MyProtocol
+                LocalAddress = $MyLocalAddress
+                LocalPort = $MyLocalPort
+                RemoteAddress = $MyRemoteAddress
+                RemotePort = $MyRemotePort
+                State = $MyState
+                PID = $MyPID
+                ProcessName = $MyProcessName
+                ProcessPath = $MyProcessPath
+                User = $MyUser
+            }
+            $Report += $LineItem
+        }
+        Return $Report
+    }
