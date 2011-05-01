2f3b450be0c15213801bd712ec04e4151bfd08e6
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-05-01 05:01:19 +0000

    Updating examples and docs.
    git-svn-id: https://mod-posh.googlecode.com/svn@270 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/NetworkManagement.ps1
+++ powershell/production/includes/NetworkManagement.ps1
@@ -72,11 +72,32 @@ Function Get-NetstatReport
     {
         <#
             .SYNOPSIS
+                Returns the output of netstat -anop TCP|UDP
             .DESCRIPTION
+                Returns the output of netstat -anop TCP|UDP in a format that can be processed by the built-in
+                PowerShell commands.
             .EXAMPLE
+                Get-NetstatReport |Format-Table
+
+                PID       ProcessNa LocalAddr State     User      ProcessPa RemoteAdd LocalPort Protocol  RemotePor
+                          me        ess                           th        ress                          t
+                ---       --------- --------- -----     ----      --------- --------- --------- --------  ---------
+                792       svchost   0.0.0.0   LISTENING                     0.0.0.0   135       TCP       0
+                4         System    0.0.0.0   LISTENING                     0.0.0.0   445       TCP       0
+                2324      vmware... 0.0.0.0   LISTENING                     0.0.0.0   912       TCP       0
+                2992      svchost   0.0.0.0   LISTENING                     0.0.0.0   990       TCP       0
+                468       wininit   0.0.0.0   LISTENING                     0.0.0.0   1025      TCP       0
+                976       svchost   0.0.0.0   LISTENING                     0.0.0.0   1026      TCP       0
+
+                Description
+                -----------
+                Sample output of the function after being piped into Format-Table
             .NOTES
+                Functionized the get-netstat code found on http://poshcode.org/get/592. The version on poshcode 
+                objectified each line and returned that line. This version creates an object above the for and adds the 
+                noteproperties inside the loop.
             .LINK
-                http://scripts.patton-tech.com/wiki/PowerShell/NetworkManagement#
+                http://scripts.patton-tech.com/wiki/PowerShell/NetworkManagement#Get-NetstatReport
         #>
         $netstat = netstat -a -n -o | where-object { $_ -match "(UDP|TCP)" }
         [regex]$regexTCP = '(?<Protocol>\S+)\s+((?<LAddress>(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?))|(?<LAddress>\[?[0-9a-fA-f]{0,4}(\:([0-9a-fA-f]{0,4})){1,7}\%?\d?\]))\:(?<Lport>\d+)\s+((?<Raddress>(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?)\.(2[0-4]\d|25[0-5]|[01]?\d\d?))|(?<RAddress>\[?[0-9a-fA-f]{0,4}(\:([0-9a-fA-f]{0,4})){1,7}\%?\d?\]))\:(?<RPort>\d+)\s+(?<State>\w+)\s+(?<PID>\d+$)'
