b1b583dba43ff56f46bfad7b279794eeafba4377
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-06-01 14:00:52 +0000

    This script will return members of each group that is found in the specified AD Path.
    git-svn-id: https://mod-posh.googlecode.com/svn@340 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/Get-AllADGroups.ps1
@@ -0,0 +1,83 @@
+<#
+    .SYNOPSIS
+        Return all group members in each group
+    .DESCRIPTION
+        This script returns all the members of each group in a provided AD Path. If you pass in just an OU
+        it will return all the groups and members in that OU.
+    .PARAMETER ADSPath
+        This is an LDAP URI in the form of LDAP://OU=something,DC=domain,DC=suffix
+    .EXAMPLE
+        .\Get-AllADGroups -ADSPath "LDAP://OU=groups,DC=company,DC=com"
+        
+        GroupPath                GroupName                UserName                 DistinguishedName
+        ---------                ---------                --------                 -----------------
+        LDAP://CN=SoftwareAcc... SoftwareAccess           Jack Tripper             CN=Jack Tripper,CN=Us...
+        LDAP://CN=SoftwareAcc... SoftwareAccess           Les Nessman              CN=Les Nessman,CN=Use...
+        LDAP://CN=Admin User ... Admin User Accounts      Ralph Monroe             CN=Ralph Monroe,CN=Us...
+        LDAP://CN=Admin User ... Admin User Accounts      Gunther Toody            CN=Gunther Toody,CN=U...
+
+        Description
+        -----------
+        Showing the basic syntax and output of the script
+    .EXAMPLE
+        .\Get-AllADGroups -ADSPath "LDAP://OU=groups,DC=company,DC=com" |Export-Csv -Path .\MyGroups.csv
+        
+        Description
+        -----------
+        This example outputs the groups and members to a csv file.
+    .NOTES
+        ScriptName: Get-AllADGroups.ps1
+        Created By: Jeff Patton
+        Date Coded: June 1, 2011
+        ScriptName is used to register events for this script
+        LogName is used to determine which classic log to write to
+    .LINK
+        http://scripts.patton-tech.com/wiki/PowerShell/Production/Get-AllADGroups
+#>
+Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$ADSPath
+    )
+Begin
+    {
+        $ScriptName = $MyInvocation.MyCommand.ToString()
+        $LogName = "Application"
+        $ScriptPath = $MyInvocation.MyCommand.Path
+        $Username = $env:USERDOMAIN + "\" + $env:USERNAME
+
+        New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
+
+        #	Dotsource in the functions you need.
+        . .\includes\ActiveDirectoryManagement.ps1
+        
+        $SecurityGroups = Get-ADObjects -ADSPath $ADSPath -SearchFilter "(objectCategory=group)"
+        $MyGroups = @()
+    }
+Process
+    {
+        foreach ($SecurityGroup in $SecurityGroups)
+        {
+            $Members = Get-ADGroupMembers -UserGroup $($SecurityGroup.Properties.name)
+            
+            foreach ($Member in $Members)
+            {
+                $ThisGroup = New-Object -TypeName PSObject -Property @{
+                    GroupName = $($SecurityGroup.Properties.name)
+                    GroupPath = $($SecurityGroup.Path)
+                    UserName = $($Member.name)
+                    DistinguishedName = $($Member.distinguishedName)
+                    }
+                $MyGroups += $ThisGroup
+                }
+            }
+    }
+End
+    {
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message
+        Return $MyGroups
+    }
