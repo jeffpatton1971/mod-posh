fae9a8d180e16288b37ce1a8997a3947deee48e1
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-06-07 21:07:28 +0000

    Checking in a script that will update permissions on GPO's for a given department, only after a full backup of all domain gpo's are made.
    git-svn-id: https://mod-posh.googlecode.com/svn@349 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/Update-DeptGPOs.ps1
@@ -0,0 +1,118 @@
+<#
+    .SYNOPSIS
+        Update permissions on Departmental GPO's
+    .DESCRIPTION
+        This script will backup all existing GPO's in the domain prior to making any changes. After the backup
+        has been made Departmental GPOs will be updated based on their Dept Code.
+    .PARAMETER DeptCode
+        A code that uniquely identifies the GPOs for your department, this is used as a filter against the 
+        name of the GPO in question.
+    .PARAMETER TargetName
+        The name of the user/group to assign permissions to
+    .PARAMETER TargetType
+        The default for this parameter is User, but if TargetName is a group, then 
+        this should be set to Group.
+    .PARAMETER PermissionLevel
+        Valid permission levels are
+            GpoRead
+            GpoApply
+            GpoEdit
+            GpoEditDeleteModifySecurity
+            None
+    .PARAMETER BackupLocation
+        A valid location either local or UNC to store the GPO backups.
+    .EXAMPLE
+        .\Update-DeptGPOs.ps1 -DeptCode "Admin" -TargetName "MyUser" -BackupLocation "c:\temp"
+        
+        Description
+        -----------
+        This example shows basic usage, and assumes the default permissionlevel of None.
+    .EXAMPLE
+        .\Update-DeptGPOs.ps1 -DeptCode "Admin" -TargetName "MyUser" -PermissionLevel "GpoEditDeleteModifySecurity" -BackupLocation "c:\temp"
+        
+        Description
+        -----------
+        This example assigns GpoEditDeleteModifySecurity to the MyUser account for all GPOs tagged as Admin
+    .NOTES
+        ScriptName: Update-DeptGPOs
+        Created By: Jeff Patton
+        Date Coded: June 7, 2011
+        ScriptName is used to register events for this script
+        LogName is used to determine which classic log to write to
+        
+        This script depends on PowerShell GroupPolicy modules to functino properly, the linked in 
+        cmdlets are used in this script.
+        
+        If a backup cannot be made the script terminates.
+    .LINK
+        http://scripts.patton-tech.com/wiki/PowerShell/Production/Update-DeptGPOs
+    .LINK
+        http://technet.microsoft.com/en-us/library/ee461059.aspx
+    .LINK
+        http://technet.microsoft.com/en-us/library/ee461038.aspx
+    .LINK
+        http://technet.microsoft.com/en-us/library/ee461052.aspx
+#>
+Param
+(
+    $DeptCode,
+    $TargetName,
+    $TargetType = "User",
+    [ValidateSet("GpoRead", "GpoApply", "GpoEdit", "GpoEditDeleteModifySecurity","None")]
+    $PermissionLevel = "None",
+    $BackupLocation
+)
+Begin
+    {
+        $ScriptName = $MyInvocation.MyCommand.ToString()
+        $LogName = "Application"
+        $ScriptPath = $MyInvocation.MyCommand.Path
+        $Username = $env:USERDOMAIN + "\" + $env:USERNAME
+
+        New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
+
+        $ErrorActionPreference = "Stop"
+        
+        Try
+        {
+            Import-Module GroupPolicy
+            $DeptGPOs = Get-GPO -All |Where-Object {$_.DisplayName -like "*$($DeptCode)*"}
+            Backup-GPO -All -Path $BackupLocation -Comment "Updating Security on $($DeptCode) GPOs"
+            }
+        Catch
+        {
+            $Message = $Error[0].Exception.InnerException.Message.ToString().Trim()
+            Write-EventLog -LogName $LogName -Source $ScriptName -EventId "102" -EntryType "Error" -Message $Message
+            
+            Write-Error $Message
+            Break
+            }
+    }
+Process
+    {
+        foreach ($DeptGPO in $DeptGPOs)
+        {
+            Try
+            {
+                Set-GPPermissions -Guid $DeptGPO.Id -TargetName $TargetName -PermissionLevel $PermissionLevel -TargetType $TargetType -WhatIf
+                $Message = "Adding $($TargetName) to $($DeptGPO.DisplayName) with permission level $($PermissionLevel)"
+                Write-EventLog -LogName $LogName -Source $ScriptName -EventId "101" -EntryType "Information" -Message $Message
+                }
+            Catch
+            {
+                $Message = $Error[0].Exception.InnerException.Message.ToString().Trim()
+                Write-EventLog -LogName $LogName -Source $ScriptName -EventId "102" -EntryType "Error" -Message $Message
+                Return $Error[0].Exception.InnerException.Message.ToString().Trim()
+                }
+            }
+    }
+End
+    {
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message	
+        
+        Return $?
+    }
