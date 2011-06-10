f9c35bc497f94ac6c7001ea7150fbd68cd9a4414
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-06-10 21:38:02 +0000

    Added Boe Prox wicked awesome FSMO function
    git-svn-id: https://mod-posh.googlecode.com/svn@357 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ActiveDirectoryManagement.ps1
+++ powershell/production/includes/ActiveDirectoryManagement.ps1
@@ -598,3 +598,56 @@ Function Add-DomainGroupToLocalGroup
 	{
 	}
 }
+Function Get-FSMORoleOwner 
+{
+    <#  
+        .SYNOPSIS  
+            Retrieves the list of FSMO role owners of a forest and domain  
+        .DESCRIPTION  
+            Retrieves the list of FSMO role owners of a forest and domain
+        .NOTES  
+            Name: Get-FSMORoleOwner
+            Author: Boe Prox
+            DateCreated: 06/9/2011  
+        .EXAMPLE
+            Get-FSMORoleOwner
+
+            DomainNamingMaster  : dc1.rivendell.com
+            Domain              : rivendell.com
+            RIDOwner            : dc1.rivendell.com
+            Forest              : rivendell.com
+            InfrastructureOwner : dc1.rivendell.com
+            SchemaMaster        : dc1.rivendell.com
+            PDCOwner            : dc1.rivendell.com
+
+            Description
+            -----------
+            Retrieves the FSMO role owners each domain in a forest. Also lists the domain and forest.        
+    #>
+
+    Try 
+    {
+        $forest = [system.directoryservices.activedirectory.Forest]::GetCurrentForest() 
+        
+        ForEach ($domain in $forest.domains) 
+        {
+            $forestproperties = @{
+                Forest = $Forest.name
+                Domain = $domain.name
+                SchemaMaster = $forest.SchemaRoleOwner
+                DomainNamingMaster = $forest.NamingRoleOwner
+                RIDOwner = $Domain.RidRoleOwner
+                PDCOwner = $Domain.PdcRoleOwner
+                InfrastructureOwner = $Domain.InfrastructureRoleOwner
+                }
+            $newobject = New-Object PSObject -Property $forestproperties
+            $newobject.PSTypeNames.Insert(0,"ForestRoles")
+            $newobject
+            }
+        }
+
+    Catch 
+    {
+        Write-Warning "$($Error)"
+        }
+}
