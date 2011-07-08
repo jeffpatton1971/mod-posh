9254f8cb41d4b080d2f7650e67af016aec6bfed2
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-07-08 14:02:05 +0000

    New function Get-SiSReport, returns sisadmin /v and basic disk info on a given disk.
    git-svn-id: https://mod-posh.googlecode.com/svn@395 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ComputerManagement.ps1
+++ powershell/production/includes/ComputerManagement.ps1
@@ -839,3 +839,76 @@ Function Export-EventLogs
         Return $?
         }
 }
+
+Function Get-SiSReport
+{
+    <#
+        .SYNOPSIS
+            Get the overall SIS usage information.
+        .DESCRIPTION
+            This function uses the sisadmin command to get the usage
+            information for a SIS enabled drive.
+        .PARAMETER SisDisk
+            The drive letter of a disk that has SiS enabled
+        .EXAMPLE
+            Get-SiSReport -SisDisk o
+
+            LinkFiles             : 20004
+            Used                  : 442378481664
+            Disk                  : o
+            InaccessibleLinkFiles : 0
+            CommonStoreFiles      : 6678
+            SpaceSaved            : 7708860 KB
+            Free                  : 0
+            
+            Description
+            -----------
+            This example shows the basic usage of the command
+
+        .NOTES
+            This function will return nothing if the drive being analyzed does not have SiS enabled
+            This function will return a message if the sisadmin command returns any error
+        .LINK
+            http://scripts.patton-tech.com/wiki/PowerShell/ComputerManagemenet#Get-SiSReport
+    #>
+    
+    Param
+    (
+    $SisDisk = "c"
+    )
+
+    Begin
+    {
+        $SisAdmin = "& sisadmin /v $($SisDisk):"
+        Try
+        {
+            $SisResult = Invoke-Expression $SisAdmin
+            }
+        Catch
+        {
+            Return "Single Instance Storage is not available on this computer"
+            }
+        }
+
+    Process
+    {
+        If ($SisResult.Count)
+        {
+            $ThisDisk = Get-PSDrive $SisDisk
+            $SisReport = New-Object -TypeName PSObject -Property @{
+                Disk = $SisDisk
+                Used = $ThisDisk.Used
+                Free = $ThisDisk.Free
+                CommonStoreFiles = ($SisResult[($SisResult.Count)-4]).TrimStart("Common store files:")
+                LinkFiles = ($SisResult[($SisResult.Count)-3]).TrimStart("Link files:")
+                InaccessibleLinkFiles = ($SisResult[($SisResult.Count)-2]).TrimStart("Inaccessible link files:")
+                SpaceSaved = ($SisResult[($SisResult.Count)-1]).TrimStart("Space saved:")
+                }
+            }
+        }
+
+    End
+    {
+        Return $SisReport
+        }
+}
