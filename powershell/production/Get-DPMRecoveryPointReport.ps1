34bc20a96c6d1f6cbcf8fe40a18375c13c7b8a4b
jeffrey <jeffrey@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2011-10-11 21:26:14 +0000

    Apparently I created two of these, copied the code from Get-!RecoveryPointReport into this file.
    git-svn-id: https://mod-posh.googlecode.com/svn@491 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/Get-DPMRecoveryPointReport.ps1
+++ powershell/production/Get-DPMRecoveryPointReport.ps1
@@ -1,37 +1,51 @@
 <#
     .SYNOPSIS
-        Get-DPMRecoveryPointReport
+        Return a list of available Recovery Points from a DPM server
     .DESCRIPTION
-        This script generates a report of all the recovery points from your DPM server.
+        This script connects to a DPM server and queries a particular Production
+        Server for it's DataSource. It then returns a list of all Recovery Points
+        available in that DataSource.
     .PARAMETER DPMServerName
-        The FQDN of your DPM server
+        The name of the server that has DPM installed on it
+    .PARAMETER ProtectedComputer
+        The name of the protected computer.
     .EXAMPLE
-        Get-DPMRecoveryPointReport -DPMServerName dpm.company.com
-        
-        Name  BackupTime             Server                   DataSource    Location
-        ----  ----------             ------                   ----------    --------
-        P:\   7/23/2011 12:01:07 AM  fs.company.com  P:\ on fs.company.com  Disk
-        P:\   7/24/2011 12:01:11 AM  fs.company.com  P:\ on fs.company.com  Disk
-        P:\   7/25/2011 12:01:44 AM  fs.company.com  P:\ on fs.company.com  Disk
-        P:\   7/26/2011 12:01:26 AM  fs.company.com  P:\ on fs.company.com  Disk
+        .\Get-DPMRecoveryPointReport.ps1 -DPMServerName 'dpm' -ProductionServerName 'fs'
+
+        RecoveryPoint             Size Server                  DataSource                     DateTime
+        -------------             ---- ------                  ----------                     --------
+        P:\           3.40232849121094 fs.cluster.soecs.ku.edu P:\ on fs.cluster.soecs.ku.edu 10/6/2011 ...
+        P:\            3.4023323059082 fs.cluster.soecs.ku.edu P:\ on fs.cluster.soecs.ku.edu 10/7/2011 ...
+        P:\            3.4023323059082 fs.cluster.soecs.ku.edu P:\ on fs.cluster.soecs.ku.edu 10/8/2011 ...
+        P:\            3.4023323059082 fs.cluster.soecs.ku.edu P:\ on fs.cluster.soecs.ku.edu 10/9/2011 ...
 
         Description
         -----------
-        The basic usage and output of this script.
+        This example shows the basic syntax of the command and expected output. The
+        Size property has been converted into Gigabytes.
     .NOTES
-        ScriptName: Get-DPMRecoveryPointReport
-        Created By: Jeff Patton
-        Date Coded: July 28, 2011
+        ScriptName : Get-DPMRecoveryPointReport.ps1
+        Created By : jspatton
+        Date Coded : 10/11/2011 10:59:47
         ScriptName is used to register events for this script
         LogName is used to determine which classic log to write to
-        
-        You will need the DPM Snap-ins in order for this to work.
+ 
+        ErrorCodes
+            100 = Success
+            101 = Error
+            102 = Warning
+            104 = Information
+
+        You could adjust this script to suit your needs, I am really only interested
+        in the date and size of a given Recovery Point.
     .LINK
-        http://scripts.patton-tech.com/wiki/PowerShell/Production/Get-DPMRecoveryPointReport
-#>
+        http://scripts.patton-tech.com/wiki/PowerShell/Production/Get-RecoveryPointReport
+ #>
+[CmdletBinding()]
 Param
     (
-    $DPMServerName = "dpm.company.com"
+        [Parameter(Mandatory=$true)]$DPMServerName = 'dpm',
+        [Parameter(Mandatory=$true)]$ProtectedComputer
     )
 Begin
     {
@@ -39,64 +53,67 @@ Begin
         $LogName = "Application"
         $ScriptPath = $MyInvocation.MyCommand.Path
         $Username = $env:USERDOMAIN + "\" + $env:USERNAME
-
+ 
         New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
-
+ 
         $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
-        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
-
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message
+ 
         #	Dotsource in the functions you need.
-
-        $Report = @()
-
-        if (Get-PSSnapin -Name 'Microsoft.DataProtectionManager.PowerShell')
-        { } 
-        else
-        { 
-            try 
+        if (!(Get-PSSnapin |Where-Object {$_.Name -eq "Microsoft.DataProtectionManager.PowerShell"}))
+        {
+            try
             {
-                Add-PSSnapin -Name 'Microsoft.DataProtectionManager.PowerShell'
+                Write-Verbose "Adding DPM Snap-in"
+                Add-PSSnapin -Name "Microsoft.DataProtectionManager.PowerShell"
                 }
-            Catch
-            { 
+            catch
+            {
                 Return $Error[0].Exception.InnerException.Message.ToString().Trim()
                 }
             }
-        $ProtectionGroups = Get-ProtectionGroup -DPMServerName $DPMServerName
-    }
+
+        Write-Verbose "Get the ProductionServer object that matches the ProtectedComputer, that actually has a DataSource."
+        $ProdServer =  Get-ProductionServer -DPMServerName $DPMServerName `
+            |Where-Object {$_.MachineName -eq $ProtectedComputer} `
+            |Where-Object {$_.IsHavingDataSourcesProtected -eq $True}
+        
+        Write-Verbose "Get a list of the DataSources available on the ProductionServer"
+        $DataSources = Get-Datasource -ProductionServer $ProdServer
+
+        $Report = @()
+        }
 Process
     {
-    If ($ProtectionGroups)
-    {
-        foreach ($ProtectionGroup in $ProtectionGroups)
+        Write-Verbose "Loop through each available DataSource"
+        foreach ($DataSource in $DataSources)
         {
-            if ($ProtectionGroup)
+            Write-Verbose "Get a list of RecoveryPoints for each DataSource"
+            $RecoveryPoints = Get-RecoveryPoint -Datasource $DataSource
+            
+            Write-Verbose "Loop through each available RecoveryPoint"
+            foreach ($RecoveryPoint in $RecoveryPoints)
             {
-                $DataSources = Get-Datasource -ProtectionGroup $ProtectionGroup
-
-                foreach ($DataSource in $DataSources)
+                Write-Verbose "Check if the RecoveryPoint is not empty"
+                if (!($RecoveryPoint -eq $null))
                 {
-                    $RecoveryPoints = Get-RecoveryPoint -Datasource $DataSource
-                    foreach ($RecoveryPoint in $RecoveryPoints)
-                    {
-                        $ThisRecoveryPoint = New-Object -TypeName PSObject -Property @{
-                            Name = $RecoveryPoint.UserFriendlyName
-                            BackupTime = $RecoveryPoint.RepresentedPointInTime
-                            DataSource = "$($RecoveryPoint.DataSource.Name) on $($RecoveryPoint.DataSource.ProductionServerName)"
-                            Server = $RecoveryPoint.DataSource.ProductionServerName
-                            Location = $RecoveryPoint.DataLocation
-                            }
-                        $Report += $ThisRecoveryPoint
+                    Write-Verbose "Pulling UserFriendlyName, RepresentedPointIntTime and Size from this RecoveryPoint"
+                    $LineItem = New-Object -TypeName PSobject -Property @{
+                        RecoveryPoint = $RecoveryPoint.UserFriendlyName;
+                        DateTime = $RecoveryPoint.RepresentedPointInTime;
+                        Size = $RecoveryPoint.Size /1gb
+                        DataSource = "$($RecoveryPoint.DataSource.Name) on $($RecoveryPoint.DataSource.ProductionServerName)"
+                        Server = $RecoveryPoint.DataSource.ProductionServerName
+                        Location = $RecoveryPoint.DataLocation
                         }
+                    $Report += $LineItem
                     }
                 }
             }
         }
-    }
 End
     {
         $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
-        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message
-        
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message	
         Return $Report
-    }
+        }
