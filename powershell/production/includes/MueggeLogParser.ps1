ef4ce2d46c6fd8a765c5fba93012d697827a079b
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-02-09 21:51:02 +0000

    Adjusting the formatting and adding the cmdlbinding option and BPE where needed
    git-svn-id: https://mod-posh.googlecode.com/svn@617 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/MueggeLogParser.ps1
+++ powershell/production/includes/MueggeLogParser.ps1
@@ -18,9 +18,8 @@
 	</Usage>
 	</Library>
 #>
-
-function Get-LPInputFormat{
-
+function Get-LPInputFormat
+{
 	<#
 		.SYNOPSIS
 			Returns Log Parser Input Format object based on passed string
@@ -37,37 +36,99 @@ function Get-LPInputFormat{
 		.LINK
 			https://code.google.com/p/mod-posh/wiki/MueggeLogParser#get-LPInputFormat
 	#>
- 
-	param([String]$InputType)
- 
-	switch($InputType.ToLower()){
-		"ads"{$inputobj = New-Object -comObject MSUtil.LogQuery.ADSInputFormat}
-		"bin"{$inputobj = New-Object -comObject MSUtil.LogQuery.IISBINInputFormat}
-		"csv"{$inputobj = New-Object -comObject MSUtil.LogQuery.CSVInputFormat}
-		"etw"{$inputobj = New-Object -comObject MSUtil.LogQuery.ETWInputFormat}
-		"evt"{$inputobj = New-Object -comObject MSUtil.LogQuery.EventLogInputFormat}
-		"fs"{$inputobj = New-Object -comObject MSUtil.LogQuery.FileSystemInputFormat}
-		"httperr"{$inputobj = New-Object -comObject MSUtil.LogQuery.HttpErrorInputFormat}
-		"iis"{$inputobj = New-Object -comObject MSUtil.LogQuery.IISIISInputFormat}
-		"iisodbc"{$inputobj = New-Object -comObject MSUtil.LogQuery.IISODBCInputFormat}
-		"ncsa"{$inputobj = New-Object -comObject MSUtil.LogQuery.IISNCSAInputFormat}
-		"netmon"{$inputobj = New-Object -comObject MSUtil.LogQuery.NetMonInputFormat}
-		"reg"{$inputobj = New-Object -comObject MSUtil.LogQuery.RegistryInputFormat}
-		"textline"{$inputobj = New-Object -comObject MSUtil.LogQuery.TextLineInputFormat}
-		"textword"{$inputobj = New-Object -comObject MSUtil.LogQuery.TextWordInputFormat}
-		"tsv"{$inputobj = New-Object -comObject MSUtil.LogQuery.TSVInputFormat}
-		"urlscan"{$inputobj = New-Object -comObject MSUtil.LogQuery.URLScanLogInputFormat}
-		"w3c"{$inputobj = New-Object -comObject MSUtil.LogQuery.W3CInputFormat}
-		"xml"{$inputobj = New-Object -comObject MSUtil.LogQuery.XMLInputFormat}
- 
-	}
- 
-	return $inputobj
- 
-}
-
-function Get-LPOutputFormat{
-
+    [CmdletBinding()]
+	param
+        (
+        [String]$InputType
+        )
+    Begin
+    {
+        }
+    Process
+    {
+	    switch($InputType.ToLower())
+        {
+		    "ads"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.ADSInputFormat
+                }
+		    "bin"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.IISBINInputFormat
+                }
+		    "csv"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.CSVInputFormat
+                }
+		    "etw"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.ETWInputFormat
+                }
+		    "evt"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.EventLogInputFormat
+                }
+		    "fs"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.FileSystemInputFormat
+                }
+		    "httperr"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.HttpErrorInputFormat
+                }
+		    "iis"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.IISIISInputFormat
+                }
+		    "iisodbc"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.IISODBCInputFormat
+                }
+		    "ncsa"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.IISNCSAInputFormat
+                }
+		    "netmon"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.NetMonInputFormat
+                }
+		    "reg"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.RegistryInputFormat
+                }
+		    "textline"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.TextLineInputFormat
+                }
+		    "textword"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.TextWordInputFormat
+                }
+		    "tsv"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.TSVInputFormat
+                }
+		    "urlscan"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.URLScanLogInputFormat
+                }
+		    "w3c"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.W3CInputFormat
+                }
+		    "xml"
+            {
+                $inputobj = New-Object -comObject MSUtil.LogQuery.XMLInputFormat
+                }
+            }
+	    }
+    End
+    {
+	    return $inputobj
+        }
+    }
+function Get-LPOutputFormat
+{
 	<#
 		.SYNOPSIS
 			Returns Log Parser Output Format object based on passed string
@@ -84,26 +145,59 @@ function Get-LPOutputFormat{
 		.LINK
 			https://code.google.com/p/mod-posh/wiki/MueggeLogParser#Get-LPOutputFormat
 	#>
-	param([String]$OutputType)
- 
-	switch($OutputType.ToLower()){
-		"csv"{$outputobj = New-Object -comObject MSUtil.LogQuery.CSVOutputFormat}
-		"chart"{$outputobj = New-Object -comObject MSUtil.LogQuery.ChartOutputFormat}
-		"iis"{$outputobj = New-Object -comObject MSUtil.LogQuery.IISOutputFormat}
-		"sql"{$outputobj = New-Object -comObject MSUtil.LogQuery.SQLOutputFormat}
-		"syslog"{$outputobj = New-Object -comObject MSUtil.LogQuery.SYSLOGOutputFormat}
-		"tsv"{$outputobj = New-Object -comObject MSUtil.LogQuery.TSVOutputFormat}
-		"w3c"{$outputobj = New-Object -comObject MSUtil.LogQuery.W3COutputFormat}
-		"tpl"{$outputobj = New-Object -comObject MSUtil.LogQuery.TemplateOutputFormat}
- 
-	}
- 
-	return $outputobj
- 
-}
-
-function Invoke-LPExecute{
-
+    [CmdletBinding()]
+	param
+        (
+        [String]$OutputType
+        )
+    Begin
+    {
+        }
+    Process
+    {
+	    switch($OutputType.ToLower())
+        {
+		    "csv"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.CSVOutputFormat
+                }
+		    "chart"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.ChartOutputFormat
+                }
+		    "iis"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.IISOutputFormat
+                }
+		    "sql"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.SQLOutputFormat
+                }
+		    "syslog"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.SYSLOGOutputFormat
+                }
+		    "tsv"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.TSVOutputFormat
+                }
+		    "w3c"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.W3COutputFormat
+                }
+		    "tpl"
+            {
+                $outputobj = New-Object -comObject MSUtil.LogQuery.TemplateOutputFormat
+                }
+            }
+        }
+    End
+    {
+	    return $outputobj
+        }
+    }
+function Invoke-LPExecute
+{
 	<#
 		.SYNOPSIS
 			Executes a Log Parser Query and returns a recordset
@@ -120,23 +214,33 @@ function Invoke-LPExecute{
 		.LINK
 			https://code.google.com/p/mod-posh/wiki/MueggeLogParser#Invoke-LPExecute
 	#>
- 
-	param([string] $query, $inputtype)
- 
-    $LPQuery = new-object -com MSUtil.LogQuery
-	if($inputtype){
-    	$LPRecordSet = $LPQuery.Execute($query, $inputtype)	
-	}
-	else
-	{
-		$LPRecordSet = $LPQuery.Execute($query)
-	}
-    return $LPRecordSet
- 
-}
-
-function Invoke-LPExecuteBatch{
-
+    [CmdletBinding()]
+	param
+        (
+        [string] $query, $inputtype
+        )
+    Begin
+    {
+        }
+    Process
+    {
+        $LPQuery = new-object -com MSUtil.LogQuery
+	    if($inputtype)
+        {
+    	    $LPRecordSet = $LPQuery.Execute($query, $inputtype)	
+	        }
+	    else
+	    {
+		    $LPRecordSet = $LPQuery.Execute($query)
+	        }
+        }
+    End
+    {
+        return $LPRecordSet
+        }
+    }
+function Invoke-LPExecuteBatch
+{
 	<#
 		.SYNOPSIS
 			Executes Log Parser batch query with passed input and output types
@@ -153,16 +257,26 @@ function Invoke-LPExecuteBatch{
 		.LINK
 			https://code.google.com/p/mod-posh/wiki/MueggeLogParser#Invoke-LPExecuteBatch
 	#>
- 
-	param([string]$query, $inputtype, $outputtype)
- 
-    $LPQuery = new-object -com MSUtil.LogQuery
-    $result = $LPQuery.ExecuteBatch($query, $inputtype, $outputtype)
-    return $result
-}
-
-function Get-LPRecord{
- 
+    [CmdletBinding()]
+	param
+        (
+        [string]$query, $inputtype, $outputtype
+        )
+    Begin
+    {
+        }
+    Process
+    {
+        $LPQuery = new-object -com MSUtil.LogQuery
+        $result = $LPQuery.ExecuteBatch($query, $inputtype, $outputtype)
+        }
+    End
+    {
+        return $result
+        }
+    }
+function Get-LPRecord
+{ 
 	<#
 		.SYNOPSIS
 			Returns PowerShell custom object from Log Parser recordset for current record
@@ -179,20 +293,30 @@ function Get-LPRecord{
 		.LINK
 			https://code.google.com/p/mod-posh/wiki/MueggeLogParser#Get-LPRecord
 	#>
-	
-	param($LPRecordSet)
- 
-	$LPRecord = new-Object System.Management.Automation.PSObject
-	if( -not $LPRecordSet.atEnd())
-	{
-		$Record = $LPRecordSet.getRecord()
-		for($i = 0; $i -lt $LPRecordSet.getColumnCount();$i++)
-		{        
-			$LPRecord | add-member NoteProperty $LPRecordSet.getColumnName($i) -value $Record.getValue($i)
-		}
-	}
-	return $LPRecord
-}
+    [CmdletBinding()]	
+	param
+        (
+        $LPRecordSet
+        )
+    Begin
+    {
+        }
+    Process
+    {
+	    $LPRecord = new-Object System.Management.Automation.PSObject
+	    if( -not $LPRecordSet.atEnd())
+	    {
+		    $Record = $LPRecordSet.getRecord()
+		    for($i = 0; $i -lt $LPRecordSet.getColumnCount();$i++)
+		    {        
+			    $LPRecord | add-member NoteProperty $LPRecordSet.getColumnName($i) -value $Record.getValue($i)
+		        }
+	        }
+    End
+    {
+	    return $LPRecord
+        }
+    }
 
 function Get-LPRecordSet{
 
@@ -212,21 +336,31 @@ function Get-LPRecordSet{
 		.LINK
 			https://code.google.com/p/mod-posh/wiki/MueggeLogParser#Get-LPRecordSet
 	#>
-	
-	param([string]$query)
- 
-	# Execute Query
-	$LPRecordSet = Invoke-LPExecute $query
-	$LPRecords = new-object System.Management.Automation.PSObject[] 0
-	for(; -not $LPRecordSet.atEnd(); $LPRecordSet.moveNext())
-	{
-		# Add record
-		$LPRecord = Get-LPRecord($LPRecordSet)
-		$LPRecords += new-Object System.Management.Automation.PSObject	
-        $RecordCount = $LPQueryResult.length-1
-        $LPRecords[$RecordCount] = $LPRecord
-	}
-	$LPRecordSet.Close();
-	return $LPRecords
- 
-}
+    [CmdletBinding()]	
+	param
+        (
+        [string]$query
+        )
+    Begin
+    {
+        }
+    Process
+    {
+	    # Execute Query
+	    $LPRecordSet = Invoke-LPExecute $query
+	    $LPRecords = new-object System.Management.Automation.PSObject[] 0
+	    for(; -not $LPRecordSet.atEnd(); $LPRecordSet.moveNext())
+	    {
+		    # Add record
+		    $LPRecord = Get-LPRecord($LPRecordSet)
+		    $LPRecords += new-Object System.Management.Automation.PSObject	
+            $RecordCount = $LPQueryResult.length-1
+            $LPRecords[$RecordCount] = $LPRecord
+	        }
+	    $LPRecordSet.Close();
+        }
+    End
+    {
+	    return $LPRecords
+        }
+    }
