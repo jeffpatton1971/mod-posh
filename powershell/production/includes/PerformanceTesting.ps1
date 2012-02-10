fc957f136332875fcec734ac4eeb8a22dd7cd799
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-02-10 03:23:39 +0000

    Adjusting the formatting and adding the cmdlbinding option and BPE where needed
    git-svn-id: https://mod-posh.googlecode.com/svn@619 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/PerformanceTesting.ps1
+++ powershell/production/includes/PerformanceTesting.ps1
@@ -1,56 +1,63 @@
 Function Get-AverageRunTime
+{
+    <#
+        .SYNOPSIS
+            Return the average time in milliseconds it takes a command to run.
+        .DESCRIPTION
+            This function returns the average time in milliseconds for a command to run over several iterations. 
+            Simply pass in the number of iterations, and the script block you wish to test.
+        .PARAMETER Counter
+            A number representing how many times to time the block.
+        .PARAMETER ScriptBlock
+            A valid cmdlet or block of code for example:
+                
+            Start-Sleep 5
+            Get-Process
+            Get-WmiObject win32_bios -ComputerName server01
+        .EXAMPLE
+            Get-AverageRunTime
+            Average time of 5 runs of 5 seconds is 5005.9882 milliseconds.
+                
+            Description
+            -----------
+            An example showing the function running with no parameters.
+        .EXAMPLE
+            Get-AverageRunTime -Counter 3
+            Average time of 3 runs of 5 seconds is 5006.80143333333 milliseconds.
+                
+            Description
+            -----------
+            An example showing the function with a different counter value.
+        .EXAMPLE
+            Get-AverageRunTime -Counter 5 -ScriptBlock "Start-Sleep 3"
+            Average time of 5 runs of (Start-Sleep 3) is 3008.26246 milliseconds
+                
+            Description
+            -----------
+            This example shows the use of both the ScriptBlock and Counter parameters.
+        .NOTES
+        .LINK
+            https://code.google.com/p/mod-posh/wiki/PerformanceTesting#Get-AverageRunTime
+    #>
+    [CmdletBinding()]
+    Param
+        (
+        $Counter = 5,
+        $ScriptBlock = 'Start-Sleep 5'
+        )
+    Begin
     {
-        <#
-            .SYNOPSIS
-                Return the average time in milliseconds it takes a command to run.
-            .DESCRIPTION
-                This function returns the average time in milliseconds for a command to run over several iterations. 
-                Simply pass in the number of iterations, and the script block you wish to test.
-            .PARAMETER Counter
-                A number representing how many times to time the block.
-            .PARAMETER ScriptBlock
-                A valid cmdlet or block of code for example:
-                
-                Start-Sleep 5
-                Get-Process
-                Get-WmiObject win32_bios -ComputerName server01
-            .EXAMPLE
-                Get-AverageRunTime
-                Average time of 5 runs of 5 seconds is 5005.9882 milliseconds.
-                
-                Description
-                -----------
-                An example showing the function running with no parameters.
-            .EXAMPLE
-                Get-AverageRunTime -Counter 3
-                Average time of 3 runs of 5 seconds is 5006.80143333333 milliseconds.
-                
-                Description
-                -----------
-                An example showing the function with a different counter value.
-            .EXAMPLE
-                Get-AverageRunTime -Counter 5 -ScriptBlock "Start-Sleep 3"
-                Average time of 5 runs of (Start-Sleep 3) is 3008.26246 milliseconds
-                
-                Description
-                -----------
-                This example shows the use of both the ScriptBlock and Counter parameters.
-            .NOTES
-            .LINK
-                https://code.google.com/p/mod-posh/wiki/PerformanceTesting#Get-AverageRunTime
-        #>
-        
-        Param
-            (
-                $Counter = 5 ,
-                $ScriptBlock = 'Start-Sleep 5'
-            )
-
+        }
+    Process
+    {
         $Counter = 0..($Counter - 1)
         $TotalMilliseconds = (
             (Measure-Command {ForEach ($Count In $Counter) 
             { Measure-Command {Invoke-Expression $ScriptBlock} }}).TotalMilliseconds / [int]$Counter.Count)
-
+        }
+    End
+    {
         Return "Average time of " + $Counter.Count + " runs of (" + $ScriptBlock + ") is " `
-                + $TotalMilliseconds + " milliseconds" 
+                + $TotalMilliseconds + " milliseconds"
+        }
     }
