9b125f320778f2439dc23c19e0b198feaf68ea80
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-02-13 19:23:36 +0000

    Added the edit-file function to the library.
    git-svn-id: https://mod-posh.googlecode.com/svn@650 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/PSISELibrary.ps1
+++ powershell/production/includes/PSISELibrary.ps1
@@ -424,3 +424,46 @@ Register-ObjectEvent -InputObject $psISE.CurrentPowerShellTab.Files CollectionCh
     {
         }
     }
+Function Edit-File
+{
+    <#
+        .SYNOPSIS
+            Open files in specified editor.
+        .DESCRIPTION
+            This function will open one or more files, in the specified editor.
+        .PARAMETER FileSpec
+            The filepath to open
+        .EXAMPLE
+            Edit-File -FileSpec c:\powershell\*.ps1
+        .NOTES
+            Set the variable $POSHEditor to the full path and filename to your editor of choice.
+        .LINK
+            https://code.google.com/p/mod-posh/wiki/PSISELibrary#Edit-File
+    #>    
+    Param
+        (
+            [Parameter(ValueFromPipeline=$true)]
+            $FileSpec
+        )
+    Begin
+        {
+            $FilesToOpen = Get-ChildItem $Filespec
+            }
+    Process
+        {
+            Foreach ($File in $FilesToOpen)
+            {
+                Try
+                {
+                    $psISE.CurrentPowerShellTab.Files.Add($File.FullName)
+                    }
+                Catch
+                {
+                    Return $Error[0].Exception
+                    }
+                }
+            }
+    End
+        {
+            }
+    }
