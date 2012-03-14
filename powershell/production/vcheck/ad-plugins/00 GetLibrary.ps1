c48f4cfbe48fb98f6df72090734cde51e352c0ce
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-03-14 16:47:04 +0000

    Committing version 1 plugins
    git-svn-id: https://mod-posh.googlecode.com/svn@706 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/vcheck/ad-plugins/00 GetLibrary.ps1
@@ -0,0 +1,6 @@
+﻿$LibFile = (Join-Path $pwd "ActiveDirectoryManagement.ps1")
+$LibURL = "http://mod-posh.googlecode.com/svn/powershell/production/includes/ActiveDirectoryManagement.ps1"
+$webclient = New-Object Net.Webclient
+$webClient.UseDefaultCredentials = $true
+$webClient.DownloadFile($LibURL, $LibFile)
+. $LibFile
