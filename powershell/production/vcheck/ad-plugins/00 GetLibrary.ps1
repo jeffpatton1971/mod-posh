99ddcab6a746c7d30e0c71dec622acbf014c7a62
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-03-21 13:45:17 +0000

    Modified the start file to give the user the option to use the stub or to download the full library from my code site.
    git-svn-id: https://mod-posh.googlecode.com/svn@717 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/vcheck/ad-plugins/00 GetLibrary.ps1
+++ powershell/production/vcheck/ad-plugins/00 GetLibrary.ps1
@@ -1,6 +1,22 @@
-﻿$LibFile = (Join-Path $pwd "ActiveDirectoryManagement.ps1")
-$LibURL = "http://mod-posh.googlecode.com/svn/powershell/production/includes/ActiveDirectoryManagement.ps1"
-$webclient = New-Object Net.Webclient
-$webClient.UseDefaultCredentials = $true
-$webClient.DownloadFile($LibURL, $LibFile)
-. $LibFile
+﻿<#
+    If you would like to have access to the full AD library I have written
+    please set $Global:LoadLibFromURL = $true.
+    
+    Otherwise this updated file will dot-source the stub library that
+    has only the functions required for the plug-in to work.
+#>
+$Global:LoadLibFromURL = $false
+
+if ($LoadLibFromURL -eq $false)
+{
+    . .\ADStubLibrary.ps1
+    }
+else
+{
+    $LibFile = (Join-Path $pwd "ActiveDirectoryManagement.ps1")
+    $LibURL = "http://mod-posh.googlecode.com/svn/powershell/production/includes/ActiveDirectoryManagement.ps1"
+    $webclient = New-Object Net.Webclient
+    $webClient.UseDefaultCredentials = $true
+    $webClient.DownloadFile($LibURL, $LibFile)
+    . $LibFile
+    }
