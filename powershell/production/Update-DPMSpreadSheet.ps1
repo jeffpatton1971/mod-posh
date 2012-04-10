121f24d2eca81affd01e4193f04e174db0077682
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-04-10 17:55:28 +0000

    Update the DPM Volume Sizing spreadsheet with current values
    git-svn-id: https://mod-posh.googlecode.com/svn@726 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/production/Update-DPMSpreadSheet.ps1
@@ -0,0 +1,175 @@
+<#
+    .SYNOPSIS
+        Update the DPM Volume Sizing spreadsheet with current values
+    .DESCRIPTION
+        This script updates the DPM Volume Sizing spreadsheet in a given
+        location with current values from one or more remote computers. The
+        spreadsheet, which is available from the Microsoft website 
+        (see RELATED LINKS), allows you to calculate the size of your Replica
+        Volume and your Recovery Point Volume based on the amount of used space
+        on a given data source.
+        
+        This script will create a session object and remotely connect to one or
+        more computers. It will then execute a scriptblock which uses Get-PSDrive
+        to return the Name and Used space of a given volume. Then for each 
+        volume it will update the appropriate column in the spreadsheet with 
+        the new used space of that disk.
+    .PARAMETER ComputerName
+        This is an array of one or more computers that are protected by
+        your DPM server.
+    .PARAMETER SharedDrives
+        This is an array of drive letters that host shares that are 
+        protected by your DPM server.
+    .PARAMETER FileName
+        This is the full path and filename to where you have extracted the 
+        DPM Volume Sizing tool.
+    .PARAMETER WorkSheetName
+        As of v3.3 WorkSheetName for file servers is
+            DPM File Volume 
+    .PARAMETER VolumeIDColumn
+        As of v3.3 VolumeIDColumn is Column
+            D:D
+    .PARAMETER TargetColumn
+        As of v3.3 TargetColumn is the Used space in GB
+            E
+    .EXAMPLE
+        .\Update-DPMSpreadSheet.ps1
+        
+        Description
+        -----------
+        This is the basic syntax of the command
+    .EXAMPLE
+        .\Update-DPMSpreadSheet.ps1 -ComputerName 'fs1.company.com','fs2.company.com' -SharedDrives 'G','W','U' -FileName 'c:\dpm\DPMvolumeSizing.xlsx'
+        
+        Description
+        -----------
+        This example shows the most common usage of this command. It will connect to fs1 and fs2
+        and update the spreadsheet in the C:\DPM folder with the appropriate values.
+    .NOTES
+        ScriptName : Update-DPMSpreadSheet.ps1
+        Created By : jspatton
+        Date Coded : 04/10/2012 11:27:08
+        ScriptName is used to register events for this script
+        LogName is used to determine which classic log to write to
+ 
+        ErrorCodes
+            100 = Success
+            101 = Error
+            102 = Warning
+            104 = Information
+        
+        I make the assumption that you have already downloaded the tool and added the appropriate
+        drive letters to the Volume Identification column. If not, you may get some odd results.
+        
+        Remember there is a max of 5 concurrent sessions via WS-MAN for a given account. If you
+        reached that number you will need to update the max. Please see the RELATED LINKS
+        for the technet blog article discussing this limit.
+    .LINK
+        https://code.google.com/p/mod-posh/wiki/Production/Update-DPMSpreadSheet
+    .LINK
+        http://blogs.technet.com/b/dpm/archive/2010/09/02/new-dpm2010-storage-calculator-links-sep-2010.aspx
+    .LINK
+        http://blogs.msdn.com/b/powershell/archive/2010/05/03/configuring-wsman-limits.aspx
+#>
+[CmdletBinding()]
+Param
+    (
+    $ComputerName = @('fs1.company.com','fs2.company.com'),
+    $SharedDrives = @('G','W','U'),
+    $FileName = 'C:\Users\jspatton\DPMvolumeSizing v3.3\DPMvolumeSizing.xlsx',
+    $WorkSheetName = 'DPM File Volume',
+    $VolumeIDColumn = 'D:D',
+    $TargetColumn = 'E'
+    )
+Begin
+    {
+        $ScriptName = $MyInvocation.MyCommand.ToString()
+        $LogName = "Application"
+        $ScriptPath = $MyInvocation.MyCommand.Path
+        $Username = $env:USERDOMAIN + "\" + $env:USERNAME
+ 
+        New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+ 
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message
+ 
+        #	Dotsource in the functions you need.
+        Write-Verbose "Does $($FileName) exist?"
+        if ((Test-Path -Path $FileName) -ne $true)
+        {
+            $Message = "The path specified is invalid. $($FileName)"
+            Write-Verbose $Message
+            Write-EventLog -LogName $LogName -Source $ScriptName -EventID "101" -EntryType "Error" -Message $Message
+            break
+            }
+        }
+Process
+    {
+        $Drives = @()
+        Write-Verbose 'Create a scriptblock that will collect the Name and used space in GB from the remote server'
+        $ScriptBlock = {Get-PSDrive -PSProvider FileSystem |Select-Object -Property Name, @{Label='Used';Expression={$_.Used /1gb}}}
+
+        try
+        {
+            foreach ($Computer in $ComputerName)
+            {
+                Write-Verbose 'Create a new session for $($Computer)'
+                $ThisSession = New-PSSession -ComputerName $Computer -Credential $Credentials
+                Write-Verbose 'Connect to the session, and collect the results of the scriptblock'
+                $Drives += Invoke-Command -Session $ThisSession -ScriptBlock $ScriptBlock
+                Write-Verbose 'Close sesion when done.'
+                Remove-PSSession -Session $ThisSession
+                }
+            }
+        catch
+        {
+            $Message = $Error[0]
+            Write-Verbose $Message
+            Write-EventLog -LogName $LogName -Source $ScriptName -EventID "101" -EntryType "Error" -Message $Message
+            Remove-PSSession -Session $ThisSession
+            break
+            }
+        try
+        {
+            Write-Verbose "Create an Excel instance"
+            $Excel = New-Object -ComObject Excel.Application
+            Write-Verbose "Open the $($FileName) spreadsheet"
+            $Excel.Workbooks.Open($FileName)
+            Write-Verbose "Open the $($WorkSheetName) worksheet"
+            $WorkSheet = $Excel.Worksheets.Item($WorkSheetName)
+            Write-Verbose "Select column $($VolumeIDColumn), the Volume Identification column"
+            $Range = $WorkSheet.Range($VolumeIDColumn)
+            
+            foreach ($SharedDrive in $SharedDrives)
+            {
+                Write-Verbose "Find $($SharedDrive) in $($VolumeIDColumn)"
+                $Target = $Range.Find($SharedDrive)
+                Write-Verbose "Found $($SharedDrive) at $($Target.AddressLocal($true,$true,$true))"
+                Write-Verbose "Get the used space for $($SharedDrive)"
+                $Used = $Drives |Where-Object {$_.Name -eq $SharedDrive} |Select-Object -Property Used
+                Write-Verbose "$($SharedDrive) : $($Used.Used)GB Used"
+                $TargetReference = "$($TargetColumn)$($Target.Row)"
+                Write-Verbose "Updating $($TargetReference) with value $($Used.Used)"
+                $WorkSheet.Range($TargetReference).Value2 = $Used.Used
+                Write-Verbose "Updated"
+                }
+            Write-Verbose "Saving $($FileName)"
+            $Excel.Save()
+            Write-Verbose "Closing $($WorkSheetName)"
+            $Excel.Workbooks.Close()
+            Write-Verbose "Exit Excel"
+            $Excel.Application.Quit()
+            }
+        catch
+        {
+            $Message = $Error[0]
+            Write-Verbose $Message
+            Write-EventLog -LogName $LogName -Source $ScriptName -EventID "101" -EntryType "Error" -Message $Message
+            break
+            }
+        }
+End
+    {
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message	
+        }
