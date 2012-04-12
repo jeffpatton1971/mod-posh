f3ea6747b1368ecb5cc56fba276cc4b3f2b2df7f
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-04-12 21:14:52 +0000

    Committing scripts for the games
    git-svn-id: https://mod-posh.googlecode.com/svn@734 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/playground/ScriptingGames/2012/AdvancedEvent3.ps1
@@ -0,0 +1,126 @@
+<#
+    .SYNOPSIS
+        Logon script to log details about user and computer
+    .DESCRIPTION
+        This script logs details about the user and computer at each logon. The data
+        is stored in a log named logonstatus.txt in the logonlog folder of the SystemDrive.
+    .EXAMPLE
+        .\AdvancedEvent3.ps1
+        
+        Description
+        -----------
+        This script is called during logon and has no parameters.
+    .EXAMPLE
+        .\AdvancedEvent3.ps1 -Verbose
+        
+        Description
+        -----------
+        Passing in the verbose switch allows you to view what is happening during
+        script execution. Useful for debugging.
+    .NOTES
+        ScriptName : AdvancedEvent3.ps1
+        Created By : jspatton
+        Date Coded : 04/04/2012 15:26:19
+        ScriptName is used to register events for this script
+        LogName is used to determine which classic log to write to
+ 
+        ErrorCodes
+            100 = Success
+            101 = Error
+            102 = Warning
+            104 = Information
+        
+        I pull the domain and hostname from Net.NetworkInformation.IPGlobalProperties because
+        the computer may be joined to a domain other than the $env:USERDNSDOMAIN
+        
+        Here is a list of things the CIO has mandated MUST be in the log file:
+
+            The user name in domainname/username format, for example: Microsoft/EdWilson 
+            The computer name in hostname.domainname format, for example: Mred.Microsoft.Com 
+            Operating system version 
+            Service Pack level 
+            All mapped drives and path to mapped resources 
+            The default printer 
+            The last reboot of the computer 
+            The type of boot up (for example, safemode or normal)
+    .LINK
+        https://code.google.com/p/mod-posh/wiki/Production/AdvancedEvent3.ps1
+#>
+[CmdletBinding()]
+Param
+    (
+    )
+Begin
+    {
+        $ScriptName = $MyInvocation.MyCommand.ToString()
+        $LogName = "Application"
+        $ScriptPath = $MyInvocation.MyCommand.Path
+        $Username = $env:USERDOMAIN + "\" + $env:USERNAME
+ 
+        New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+ 
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message
+ 
+        #	Dotsource in the functions you need.
+        $LogFileName = 'logonstatus.txt'
+        $LogLocation = '\logonlog'
+        
+        if (!(Test-Path "$($env:SystemDrive)$($LogLocation)"))
+        {
+            New-Item "$($env:SystemDrive)$($LogLocation)" -ItemType Directory |Out-Null
+            }
+        $FileName = "$($env:SystemDrive)$($LogLocation)\$($LogFileName)"
+        }
+Process
+    {
+        $Message = "Connect to Win32_OperatingSystem to get LastBootUpTime"
+        Write-Verbose $Message
+        $LastBoot = Get-WMIObject -class Win32_OperatingSystem
+        Write-Verbose $LastBoot.ConvertToDateTime($LastBoot.LastBootUpTime)
+        
+        $Message = "This will return the hostname and domainname of the computer"
+        Write-Verbose $Message
+        $ipProperties = [System.Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties()
+        Write-Verbose $ipProperties
+        
+        $Message = "Connect to Win32_MappedLogicalDisk to return Name and ProviderName"
+        Write-Verbose $Message
+        $MappedDrives = Get-WmiObject -Class Win32_MappedLogicalDisk |Select-Object @{Name='Drive Letter';Expression={$_.Name}},@{Name='Resource Path';Expression={$_.ProviderName}}
+        Write-Verbose "Converted Name to Drive Letter and ProviderName to Resource Path"
+        Write-Verbose $MappedDrives.ToString()
+        
+        $Message = "Connect to Win32_Printer and get the printer object that is set as default"
+        Write-Verbose $Message
+        $DefaultPrinter = Get-WmiObject -Class Win32_Printer -Filter 'Default = TRUE'
+        Write-Verbose $DefaultPrinter
+        
+        $Message = "Connect to Win32_OperatingSystem to get Version and CSDVersion"
+        Write-Verbose $Message
+        $OsDetails = Get-WmiObject -Class Win32_OperatingSystem
+        Write-Verbose $OsDetails
+        
+        $Message = "Connect to Win32_ComputerSystem to get BootupState"
+        Write-Verbose $Message
+        $ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem
+        Write-Verbose $ComputerSystem.BootupState
+        
+        $Report = New-Object -TypeName PSObject -Property @{
+            LastReboot = $LastBoot.ConvertToDateTime($LastBoot.LastBootUpTime)
+            ComputerName = "$($ipProperties.HostName).$($ipProperties.DomainName)"
+            UserName = $Username
+            OperatingSystemVersion = $OsDetails.Version
+            CurrentLog = Get-Date
+            OperatingSystemServicePack = $OsDetails.CSDVersion
+            DefaultPrinter = $DefaultPrinter.Name
+            Drive = $MappedDrives
+            TypeOfBoot = $ComputerSystem.BootupState
+            }
+        }
+End
+    {
+        $Report |Select-Object -Property LastReboot, ComputerName, UserName, OperatingSystemVersion, CurrentLog, OperatingSystemServicePack, DefaultPrinter, Drive, TypeOfBoot `
+                |Out-File -FilePath $FileName -Append
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message	
+        }
---
+++ powershell/playground/ScriptingGames/2012/AdvancedEvent9.ps1
@@ -0,0 +1,191 @@
+<#
+    .SYNOPSIS
+        A script to inventory the local computer
+    .DESCRIPTION
+        This script will collect the following information: computer name, domain name, computer 
+        manufacturer, computer model, number of processors, number of cores, speed of processors, 
+        processor ID, MAC address of the primary network interface, operating system version 
+        (including service pack level), and the amount of physical memory that is installed 
+        (displayed in the most logical units).
+        
+        The output should be stored in an XML-formatted file in the Documents special folder.
+    .PARAMETER ComputerName
+        The name of the computer
+    .EXAMPLE
+        .\AdvancedEvent9.ps1
+        
+        Description
+        -----------
+        This is the basic syntax of the command, there is no output by default.
+    .NOTES
+        ScriptName : AdvancedEvent9.ps1
+        Created By : jspatton
+        Date Coded : 04/12/2012 11:10:30
+        ScriptName is used to register events for this script
+        LogName is used to determine which classic log to write to
+ 
+        ErrorCodes
+            100 = Success
+            101 = Error
+            102 = Warning
+            104 = Information
+        
+        There are some things I had to take into account when writing this, first was multi-homed
+        machines. I have several machines that I manage that don't have a single primary interface
+        so for the mac address, you will see more than one item if you have more than one physical 
+        adapter. That's the other thing, how do you determine a physical adapter? I found a nice
+        little article that talked about checking the PNPDeviceID for non ROOT values, that's the
+        option I chose, the URL is in Related Links.
+        
+        Another item that came up in testing was single proc multi-cores versus multi-proc mult-
+        cores. My desktop has one physical CPU with 4 cores, but another machine has two physical
+        procs with 8 cores. If WMI returns a non-array I assume 1 proc and move on with my life,
+        if WMI returns an array, I loop through the array and add up the number of cores total.
+        
+        To calculate RAM appropriately an if statement was the best I could come up with, I don't
+        know that it's the best method of doing this, but it seems to work. I had to use some .NET
+        classes since there is no power operator in PowerShell, at least that I could find. I also 
+        used rounding to clean it up a little.
+    .LINK
+        https://code.google.com/p/mod-posh/wiki/Playground/ScriptingGames/2012/AdvancedEvent9.ps1
+    .LINK
+        http://weblogs.sqlteam.com/mladenp/archive/2010/11/04/find-only-physical-network-adapters-with-wmi-win32_networkadapter-class.aspx
+#>
+[CmdletBinding()]
+Param
+    (
+    $ComputerName = '.'
+    )
+Begin
+    {
+        $ScriptName = $MyInvocation.MyCommand.ToString()
+        $LogName = "Application"
+        $ScriptPath = $MyInvocation.MyCommand.Path
+        $Username = $env:USERDOMAIN + "\" + $env:USERNAME
+ 
+        New-EventLog -Source $ScriptName -LogName $LogName -ErrorAction SilentlyContinue
+ 
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message
+ 
+        #	Dotsource in the functions you need.
+        try
+        {
+            Write-Verbose 'Get details about computer make, model and ram.'
+            $ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $ComputerName -ErrorAction Stop
+            
+            Write-Verbose 'Get details about the processor'
+            $Processor = Get-WmiObject -Class Win32_Processor -ComputerName $ComputerName -ErrorAction Stop
+            
+            Write-Verbose 'Determine the physical adapter'
+            $Adapter = Get-WmiObject -Class Win32_NetworkAdapter -Filter "NetEnabled = True AND NOT PNPDeviceID LIKE 'ROOT\\%'" -ComputerName $ComputerName -ErrorAction Stop
+            
+            Write-Verbose 'Get OperatingSystem information'
+            $OS = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $ComputerName -ErrorAction Stop
+            }
+        catch
+        {
+            $Message = $Error[0]
+            Write-Verbose $Message
+            Write-EventLog -LogName $LogName -Source $ScriptName -EventID "101" -EntryType "Error" -Message $Message
+            }
+        Write-Verbose 'Create filename'
+        $FileName = "$($ComputerSystem.Name).$($ComputerSystem.Domain).$((get-date -format "yyyMMdd")).xml"
+        }
+Process
+    {
+        Write-Verbose 'Calculating amount of ram'
+        if ($ComputerSystem.TotalPhysicalMemory -gt [math]::Pow(1024,3))
+        {
+            Write-Verbose 'More than 1GB of ram installed'
+            $PhysicalRam = "$([math]::round($ComputerSystem.TotalPhysicalMemory/[math]::Pow(1024,3))) GB"
+            }
+        if ($ComputerSystem.TotalPhysicalMemory -le [math]::Pow(1024,3) -and $ComputerSystem.TotalPhysicalMemory -ge [math]::Pow(1024,2))
+        {
+            Write-Verbose 'Less than 1GB of ram installed'
+            $PhysicalRam = "$([math]::round($ComputerSystem.TotalPhysicalMemory/[math]::Pow(1024,2))) MB"
+            }
+        
+        if ($Processor.Count -eq $null)
+        {
+            if ($Core.NumberOfCores -ne $null)
+            {
+                Write-Verbose 'One processor found'
+                $Cores = $Processor.NumberOfCores
+                }
+            else
+            {
+                Write-Verbose "This version of Windows doesn't support NumberOfCores property"
+                $Cores = 0
+                }
+            }
+        else
+        {
+            Write-Verbose 'More than one processor found'
+            foreach ($Core in $Processor)
+            {
+                if ($Core.NumberOfCores -ne $null)
+                {
+                    Write-Verbose 'Calculate total number of cores.'
+                    $Cores += $Core.NumberOfCores
+                    }
+                else
+                {
+                    Write-Verbose "This version of Windows doesn't support NumberOfCores property"
+                    $Cores = 0
+                    }                
+                Write-Verbose 'Calculate total clockspeed'
+                $ClockSpeed += $Core.CurrentClockSpeed
+                
+                Write-Verbose 'Get the processor id'
+                if ($Core.ProcessorId -ne $null)
+                {
+                    $ProcessorID = $Core.ProcessorId
+                    }
+                }
+            }
+        Write-Verbose 'Pull in the MAC address, this could contain an array on multi-homed machines'
+        $MacAddress = $Adapter |Select-Object -Property MACAddress -ExpandProperty MACAddress
+        
+        Write-Verbose 'Determine ServicePack level'
+        if ($OS.ServicePackMajorVersion -gt 0)
+        {
+            $OSCaption = "$($OS.Version) SP$($OS.ServicePackMajorVersion)"
+            }
+        else
+        {
+            $OSCaption = $OS.Version
+            }
+        
+        Write-Verbose 'Build the object to export out'
+        $InventoryReport = New-Object -TypeName PSobject -Property @{
+            ComputerName = $ComputerSystem.Name
+            DomainName = $ComputerSystem.Domain
+            Manufacturer = $ComputerSystem.Manufacturer
+            Model = $ComputerSystem.Model
+            NumProcessors = $ComputerSystem.NumberOfProcessors
+            NumCores = $Cores
+            ClockSpeed = $ClockSpeed/$ComputerSystem.NumberOfProcessors
+            ProcessorID = $ProcessorId
+            MACAddress = $MACAddress
+            OperatingSystemVersion = $OSCaption
+            PhysicalRam = $PhysicalRam
+            }
+        }
+End
+    {
+        Write-Verbose 'Store inventory report in the Documents folder'
+        try
+        {
+            $InventoryReport |Select-Object -Property ComputerName, DomainName, Manufacturer, Model, NumProcessors, NumCores, ClockSpeed, ProcessorID, MACAddress, OperatingSystemVersion, PhysicalRam `
+             |Export-Clixml -Path "$([environment]::getfolderpath("mydocuments"))\$($FileName)"  -ErrorAction Stop
+            }
+        catch
+        {
+            $Message = $Error[0]
+            Write-Verbose $Message
+            Write-EventLog -LogName $LogName -Source $ScriptName -EventID "101" -EntryType "Error" -Message $Message	
+            }
+        $Message = "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nFinished: " + (Get-Date).toString()
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "104" -EntryType "Information" -Message $Message	
+        }
