ff8099140c9f941b722b0cc2adb770a954f5a75e
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-04-30 22:03:38 +0000

    Replaced the old eventlog export function with a new and improved version.
    git-svn-id: https://mod-posh.googlecode.com/svn@769 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/ComputerManagement.ps1
+++ powershell/production/includes/ComputerManagement.ps1
@@ -821,48 +821,159 @@ Function Backup-EventLogs
         Return $?
         }
     }
-Function Export-EventLogs
+Function Export-EventLog
 {
     <#
         .SYNOPSIS
-            Export Eventlogs from remote computer
+            Export an Eventlog from a local or remote computer
         .DESCRIPTION
-            This function backs up all logs on a Windows computer that have events written in them. This
-            log is stored as a .csv file in the current directory, where the filename is the ComputerName+
-            Logname+Date+Time the backup was created.
+            This function will export the logname you specify to the folder
+            and filename that you provide. The exported file is in the native
+            format for Event logs. 
+            
+            This function leverages the System.Diagnostics.Eventing.Reader class
+            to export the log of the local or remote computer.
         .PARAMETER ComputerName
-            The NetBIOS name of the computer to connect to.
+           Type the NetBIOS name, an Internet Protocol (IP) address, or the fully 
+           qualified domain name of the computer. The default value is the local 
+           computer.
+
+           This parameter accepts only one computer name at a time. To find event logs 
+           or events on multiple computers, use a ForEach statement. 
+           
+           To get events and event logs from remote computers, the firewall port for 
+           the event log service must be configured to allow remote access.
+        .PARAMETER Credential
+            Specifies a user account that has permission to perform this action. The 
+            default value is the current user.
+        .PARAMETER ListLog
+            If present the function will list all the logs currently available on the
+            computer.
+        .PARAMETER LogName
+            Export messages from the specified LogName
+        .PARAMETER Destination
+            The full path and filename to where the log should be exported to.
         .EXAMPLE
-            Export-EventLogs -ComputerName dc1
+            Export-EventLogs -ComputerName sql -Credential (Get-Credential) -LogName Application -Destination 'C:\LogFiles1\Application.evtx'
+            
+            Description
+            -----------
+            This example shows how to export the Application log from a computer named SQL and save
+            the file as Application.evtx in a folder called LogFiles. This also shows how to use 
+            the Get-Credential cmdlet to pass credentials into the function.
+        .EXAMPLE
+            Export-EventLog -ListLog
+            Application
+            HardwareEvents
+            Internet Explorer
+            Key Management Service
+            Media Center
+            
+            Description
+            -----------
+            This example shows how to list the lognames on the local computer
+        .EXAMPLE
+            Export-EventLog -LogName Application -Destination C:\Logs\App.evtxExport-EventLog -LogName Application -Destination C:\Logs\App.evtx
+            
+            Description
+            -----------
+            This example shows how to export the Application log on the local computer to
+            a folder on the local computer.
         .NOTES
-            May need to be a user with rights to access various logs, such as security on remote computer.
+            FunctionName : Export-EventLogs
+            Created by   : jspatton
+            Date Coded   : 04/30/2012 12:36:12
+            
+            The folder and filename that you specify will be created on the remote machine.
         .LINK
-            https://code.google.com/p/mod-posh/wiki/ComputerManagement#Export-EventLogs
+            https://code.google.com/p/mod-posh/wiki/ComputerManagement#Export-EventLog
     #>
     [CmdletBinding()]
-    Param
-        (
-        [string]$ComputerName
-        )
+     Param
+         (
+         $ComputerName,
+         $Credential,
+         [switch]$ListLog,
+         $LogName,
+         $Destination
+         )
     Begin
     {
-        $EventLogs = Get-WinEvent -ListLog * -ComputerName $ComputerName
+        if (!($ComputerName))
+        {
+            Write-Verbose "No ComputerName passed, setting ComputerName to $(& hostname)"
+            $ComputerName = (& hostname)
+            }
+        if ($Credential)
+        {
+            Write-Verbose "Attempting to connect to $($ComputerName) as $($Credential.Username)"
+            $EventSession = New-Object System.Diagnostics.Eventing.Reader.EventLogSession($ComputerName, `
+                                                                                          $Credential.GetNetworkCredential().Domain, `
+                                                                                          $Credential.GetNetworkCredential().Username, `
+                                                                                          $Credential.Password,'Default')
+            
+            Write-Verbose "Checking to see if \\$($ComputerName)\$((([System.IO.Directory]::GetParent($Destination)).FullName).Replace(":","$")) exists"
+            if (!($ListLog) -and (Test-Path -Path "\\$($ComputerName)\$((([System.IO.Directory]::GetParent($Destination)).FullName).Replace(":","$"))") -ne $true)
+            {
+                Write-Verbose "Creating $((([System.IO.Directory]::GetParent($Destination)).FullName).Replace(":","$"))"
+                $ScriptBlock = {New-Item -Path $args[0] -ItemType Directory -Force}
+                Invoke-Command -ScriptBlock $ScriptBlock -ComputerName $ComputerName -Credential $Credential -ArgumentList (([System.IO.Directory]::GetParent($Destination)).FullName) |Out-Null
+                }
+            }
+        else
+        {
+            Write-Verbose "Checking to see if $($Destination) exists."
+            if (!($ListLog) -and (Test-Path $Destination) -ne $true)
+            {
+                Write-Verbose "Creating $((([System.IO.Directory]::GetParent($Destination)).FullName).Replace(":","$"))"
+                New-Item -Path (([System.IO.Directory]::GetParent($Destination)).FullName) -ItemType Directory -Force |Out-Null
+                }
+            Write-Verbose "Connecting to $($ComputerName)"
+            $EventSession = New-Object System.Diagnostics.Eventing.Reader.EventLogSession($ComputerName)
+            }
         }
     Process
     {
-        Foreach ($EventLog in $EventLogs)
+        switch ($ListLog)
         {
-            If ($EventLog.RecordCount -gt 0)
+            $true
             {
-                $LogName = ($EventLog.LogName).Replace("/","-")
-                $BackupFilename = "$($ComputerName)-$($LogName)-"+(Get-Date -format "yyy-MM-dd HH-MM-ss").ToString()+".csv"
-                Get-WinEvent -LogName $EventLog.LogName -ComputerName $ComputerName |Export-Csv -Path ".\$($BackupFilename)"
+                try
+                {
+                    Write-Verbose "Outputting a list of all lognames"
+                    $EventSession.GetLogNames()
+                    }
+                catch
+                {
+                    Write-Error $Error[0]
+                    break
+                    }
+                }
+            $false
+            {
+                try
+                {
+                    if (($EventSession.GetLogNames() |Where-Object {$_ -eq $LogName}) -eq $null)
+                    {
+                        Write-Error "There is not an event log on the $($ComputerName) computer that matches `"$($LogName)`""
+                        }
+                    else
+                    {
+                        Write-Verbose "Exporting event log $($LogName) to the following location $($Destination)"
+                        $EventSession.ExportLogAndMessages($LogName,'LogName','*',$Destination)
+                        }
+                    }
+                catch
+                {
+                    Write-Error $Error[0]
+                    break
+                    }
                 }
             }
+        
         }
     End
     {
-        Return $?
         }
     }
 Function Get-SiSReport
