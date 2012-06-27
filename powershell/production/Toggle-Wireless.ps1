9f08ff8eadd78476f702145bc476a199309bd53a
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-06-27 21:43:23 +0000

    Fleshed out the inline documentation
    git-svn-id: https://mod-posh.googlecode.com/svn@813 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/Toggle-Wireless.ps1
+++ powershell/production/Toggle-Wireless.ps1
@@ -1,10 +1,33 @@
 <#
     .SYNOPSIS
-        Template script
+        This script will toggle the wireless adapter on or off based on batterystatus
     .DESCRIPTION
-        This script sets up the basic framework that I use for all my scripts.
-    .PARAMETER
+        This script will query Win32_Battery and will evaluate the value of the 
+        BatteryStatus property to determine what to do. There are 11 possible values
+        a value of 2 states the system has access to AC, so I use this as the
+        value to check.
+        
+        If BatteryStatus is 2, the script will disable the wireless adapter that has
+        the matching ConnectionID. Any other value then the script will enable the
+        same wireless adapter.
+        
+        For a complete list of values please see the related links section.
+    .PARAMETER ConnectionID
+        This is a string that represents how the network card is named when you view
+        it from the Network and Sharing applet in Windows. This is stored as the
+        NetConnectionID in WMI.
+        
+        This value must match how the network card is displayed otherwise the script
+        will fail. For information on changing the name of your network adapter
+        please see the related links section.
     .EXAMPLE
+        .\Toggle-Wireless.ps1 -ConnectionID 'Wifi'
+        
+        Description
+        -----------
+        This example shows the basic syntax of the command. If there is an adapter
+        with a NetConnectionID of Wifi, then based on the value of BatteryStatus
+        the adapter will either be enabled or disabled.
     .NOTES
         ScriptName : Toggle-Wireless.ps1
         Created By : jspatton
@@ -16,8 +39,14 @@
             101 = Error
             102 = Warning
             104 = Information
+        
+        This script needs to run from an administrative shell. 
     .LINK
         https://code.google.com/p/mod-posh/wiki/Production/Toggle-Wireless.ps1
+    .LINK
+        http://msdn.microsoft.com/en-us/library/windows/desktop/aa394074(v=vs.85).aspx
+    .LINK
+        http://technet.microsoft.com/en-us/library/dd163571
 #>
 [CmdletBinding()]
 Param
