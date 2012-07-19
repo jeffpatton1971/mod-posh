371273292e16f960dc84d11a4fa8e7dad0df7b61
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-07-19 03:38:36 +0000

    Updating scripts to use the new modules
    git-svn-id: https://mod-posh.googlecode.com/svn@849 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/Get-AllADGroups.ps1
+++ powershell/production/Get-AllADGroups.ps1
@@ -52,7 +52,16 @@ Begin
         Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
 
         #	Dotsource in the functions you need.
-        . .\includes\ActiveDirectoryManagement.ps1
+        Try
+        {
+            Import-Module .\includes\ActiveDirectoryManagement.psm1
+            }
+        Catch
+        {
+            Write-Warning "Must have the ActiveDirectoryManagement Module available."
+            Write-EventLog -LogName $LogName -Source $ScriptName -EventID "101" -EntryType "Error" -Message "ActiveDirectoryManagement Module Not Found"
+            Break
+            }
         
         $SecurityGroups = Get-ADObjects -ADSPath $ADSPath -SearchFilter "(objectCategory=group)"
         $MyGroups = @()
