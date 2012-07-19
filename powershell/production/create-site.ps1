d13a7d6d4cf9e25f9a9f80e7602e6268ba85aa4e
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-07-19 03:38:28 +0000

    Updating scripts to use the new modules
    git-svn-id: https://mod-posh.googlecode.com/svn@846 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/create-site.ps1
+++ powershell/production/create-site.ps1
@@ -40,7 +40,16 @@ Begin
     Write-EventLog -LogName $LogName -Source $ScriptName -EventID "100" -EntryType "Information" -Message $Message 
 
     #    Dotsource in the SharePoint functions I need
-    . .\includes\SharePointManagement.ps1
+    Try
+    {
+        Import-Module .\includes\SharePointManagement.psm1
+        }
+    Catch
+    {
+        Write-Warning "Must have the SharePointManagement Module available."
+        Write-EventLog -LogName $LogName -Source $ScriptName -EventID "101" -EntryType "Error" -Message "SharePointManagement Module Not Found"
+        Break
+        }
     }
 Process
 {
