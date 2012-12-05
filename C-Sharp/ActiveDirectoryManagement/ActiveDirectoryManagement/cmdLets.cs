aba480665ca0919739b7603624fc2e4aefd7fee2
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2012-12-05 17:58:14 +0000

    Checking in the existing code for ActiveDirectoryManagement
    git-svn-id: https://mod-posh.googlecode.com/svn@931 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ C-Sharp/ActiveDirectoryManagement/ActiveDirectoryManagement/cmdLets.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Management.Automation;
+using Utilities;
+
+namespace ActiveDirectoryManagement
+{
+    [System.Management.Automation.Cmdlet(System.Management.Automation.VerbsCommon.Get,"AdObject")]
+    public class Get_AdObject: System.Management.Automation.PSCmdlet
+    {
+        [System.Management.Automation.Parameter(Position = 0, Mandatory = false)]
+        public string Path;
+
+        [System.Management.Automation.ValidateSet("computer", "user", "group", "organizationalunit")]
+        [System.Management.Automation.Parameter(Position = 1, Mandatory = false)]
+        public string Type;
+
+        [System.Management.Automation.Parameter(Position = 2, Mandatory = false)]
+        public string Filter;
+
+        [System.Management.Automation.ValidateSet("Base","OneLevel","Subtree")]
+        [System.Management.Automation.Parameter(Position = 3, Mandatory = false)]
+        public string Scope;
+
+        [System.Management.Automation.Parameter(Position = 4, Mandatory = false)]
+        public string[] Properties;
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            if (Path == null)
+            {
+                WriteVerbose("No Path set on cmdline, setting Path to current domain");
+                DirectoryEntry myDir = new DirectoryEntry();
+                Path = myDir.Path;
+
+                WriteDebug("Close directory entry object");
+                myDir.Dispose();
+            }
+
+            if (Type == null)
+            {
+                WriteVerbose("No Type set on cmdline, setting Type to computer");
+                Type = "computer";
+            }
+
+            if (Filter == null)
+            {
+                WriteVerbose("No Filter set on cmdline, using the value of Type to build filter");
+                switch (Type)
+                {
+                    case "computer":
+                        {
+                            WriteVerbose("Using generic LDAP search for " + Type);
+                            Filter = "(objectCategory=computer)";
+                            break;
+                        }
+                    case "user":
+                        {
+                            WriteVerbose("Using generic LDAP search for " + Type); 
+                            Filter = "(objectCategory=user)";
+                            break;
+                        }
+                    case "group":
+                        {
+                            WriteVerbose("Using generic LDAP search for " + Type); 
+                            Filter = "(objectCategory=group)";
+                            break;
+                        }
+                    case "organizationalunit":
+                        {
+                            WriteVerbose("Using generic LDAP search for " + Type); 
+                            Filter = "(objectCategory=organizationalunit)";
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                } // End Switch
+            } // End If
+
+            if (Scope == null)
+            {
+                WriteVerbose("No Scope set on cmdline, setting Scope to Subtree");
+                Scope = "Subtree";
+            }
+        }
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            try
+            {
+                foreach (SearchResult AdObject in (Utilities.Functions.QueryAD(Path, Filter, Scope, Properties)))
+                {
+                    WriteVerbose("Create PowerShell object to hold return values");
+                    PSObject objReturn = new PSObject();
+
+                    WriteVerbose("Add AdObject.Properties to PowerShell object");
+                    foreach (string AdProperty in AdObject.Properties.PropertyNames)
+                    {
+                        WriteDebug("Add property : " + AdProperty);
+                        objReturn.Properties.Add(new PSNoteProperty(AdProperty, (AdObject.Properties[AdProperty])[0]));
+                    }
+                    WriteObject(objReturn);
+                }
+            } // End Try
+            catch
+            {
+            }
+        }
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+}
---
+++ C-Sharp/ActiveDirectoryManagement/ActiveDirectoryManagement/utilities.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Management.Automation;
+
+namespace Utilities
+{
+    public class Functions
+    {
+        public static SearchResultCollection QueryAD(string ldapPath, string ldapFilter, string searchScope, string[] ldapProperties)
+        {
+            if (ldapPath == null)
+            {
+                // No Path set on cmdline, setting Path to current domain
+                DirectoryEntry myDir = new DirectoryEntry();
+                ldapPath = myDir.Path;
+
+                // Close directory entry object
+                myDir.Dispose();
+            }
+            else if (!(ldapPath.ToUpper().Contains("LDAP://")))
+            {
+                ldapPath = "LDAP://" + ldapPath;
+            }
+
+            if (ldapFilter == null)
+            {
+                // Using generic LDAP search for computer
+                ldapFilter = "(objectCategory=computer)";
+            }
+
+            if (searchScope == null)
+            {
+                // No Scope set on cmdline, setting Scope to Subtree
+                searchScope = "Subtree";
+            }
+
+            if (ldapPath.ToUpper().Contains("CN="))
+            {
+                ldapFilter = "";
+                searchScope = "Base";
+            }
+
+            try
+            {
+                // Creating new DirectoryEntry object
+                DirectoryEntry dirEntry = new DirectoryEntry(ldapPath.ToUpper());
+                // Creating new DirectorySearcher
+                DirectorySearcher dirSearcher = new DirectorySearcher(ldapFilter);
+
+                // Setting SearchRoot
+                dirSearcher.SearchRoot = dirEntry;
+                // Setting PageSize to 1000
+                dirSearcher.PageSize = 1000;
+                // Setting dirSearcher filter
+                dirSearcher.Filter = ldapFilter;
+
+                switch (searchScope)
+                {
+                    case "Base":
+                        {
+                            // Setting Searchscope to Base
+                            dirSearcher.SearchScope = SearchScope.Base;
+                            break;
+                        }
+                    case "OneLevel":
+                        {
+                            // Setting Searchscope to OneLevel
+                            dirSearcher.SearchScope = SearchScope.OneLevel;
+                            break;
+                        }
+                    case "Subtree":
+                        {
+                            // Setting Searchscope to Subtree
+                            dirSearcher.SearchScope = SearchScope.Subtree;
+                            break;
+                        }
+                } // End Switch
+
+                if (ldapProperties != null)
+                {
+                    // Load properties into the query
+                    foreach (string AdProperty in ldapProperties)
+                    {
+                        // Loading property
+                        dirSearcher.PropertiesToLoad.Add(AdProperty);
+                    }
+                }
+                dirEntry.Dispose();
+                SearchResultCollection queryResult;
+                queryResult = dirSearcher.FindAll();
+                return queryResult;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
