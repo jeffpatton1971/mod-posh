70bd4625392c966bbebd2d3dd3cd5a86ca9eb58f
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2013-07-10 21:26:00 +0000

    A rough draft of code to update SCCM when sources change
    git-svn-id: https://mod-posh.googlecode.com/svn@940 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ powershell/playground/Update-SCCMSources.ps1
@@ -0,0 +1,17 @@
+﻿$SiteCode = "sms"
+$Source = "\\server1\APPS"
+$Destination = "\\server2\APPS"
+
+$Packages = Get-WMIObject -namespace "root\sms\site_$($SiteCode)" -class SMS_Package
+foreach ($Package in $Packages) 
+{
+    $Id = $Package.PackageID
+    $ThisPackage = Get-WMIObject -namespace "root\sms\site_$($SiteCode)" -class SMS_Package -filter "PackageID=$($Id)"
+
+    foreach ($Item in $ThisPackage) 
+    {
+        $Path = $Item.PkgSourcePath.Replace($Source, $Destination)
+        $Item.PkgSourcePath = $Path
+        $Item.Put()
+        }
+    }
