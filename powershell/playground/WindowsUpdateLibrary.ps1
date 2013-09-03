a5131cd7de6dbf19b56dd0366b70d8f331cf3f67
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2013-09-03 15:00:43 +0000

    Added inline help
    git-svn-id: https://mod-posh.googlecode.com/svn@967 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/playground/WindowsUpdateLibrary.ps1
+++ powershell/playground/WindowsUpdateLibrary.ps1
@@ -2,15 +2,41 @@
 {
     <#
         .SYNOPSIS
+            Get a list of updates from an update server
         .DESCRIPTION
+            This function will return a list of updates from either a local WSUS update
+            server, or via switch from Microsoft Windows Updates.
+
+            This function leverages the Microsoft.Update comobjects in order to provide
+            an easy way for administrators to script update management.
         .PARAMETER Criteria
+            This is a specialized query for getting a list of updates. The default setting
+            will return a list of software updates that are not installed. Pleae see the 
+            Link section for a URL that documents that syntax for you.
+        .PARAMETER FromMicrosoft
+            This switch if present will force the WUA agent to talk directly to 
+            Microsoft Update servers.
         .EXAMPLE
+            Get-WindowsUpdate
+
+            Description
+            -----------
+            This shows the basic syntax, the function will return an UpdateCollection object
+            containing one or more available updates based on your criteria.
+        .EXAMPLE
+            Get-WindowsUpdate -FromMicrosoft
+
+            Description
+            -----------
+            This shows the syntax for using the FromMicrosoft switch, the collection returned
+            from this example will be direct from Microsoft. You can verify by checking the
+            log file, C:\Windows\WindowsUpdate.log
         .NOTES
-            FunctionName : .\Get-WindowsUpdateLog.ps1
+            FunctionName : Get-WindowsUpdate
             Created by   : jspatton
             Date Coded   : 08/22/2013 16:40:27
         .LINK
-            https://code.google.com/p/mod-posh/wiki/Untitled20#.\Get-WindowsUpdateLog.ps1
+            https://code.google.com/p/mod-posh/wiki/WindowsUpdateLibrary#Get-WindowsUpdate
     #>
     [CmdletBinding()]
     Param
@@ -40,15 +66,29 @@ Function Start-WindowsUpdateDownload
 {
     <#
         .SYNOPSIS
+            Start downloading available updates from an update server
         .DESCRIPTION
+            This function takes an UpdateCollection and proceeds to download those updates
+            from the server.
+
+            This function leverages the Microsoft.Update comobjects in order to provide
+            an easy way for administrators to script update management.
         .PARAMETER Update
+            This is a collection of updates as returned from an update server.
         .EXAMPLE
+            Get-WindowsUpdate |Start-WindowsUpdateDownload
+
+            Description
+            -----------
+            This example shows the best way to use this function. The output of Get-WindowsUpdate
+            as passed along the pipeline to Start-WindowsUpdateDownload. This will get the most
+            recent list of updates and download them to the local computer.
         .NOTES
             FunctionName : Start-WindowsUpdateDownload
             Created by   : jspatton
             Date Coded   : 08/23/2013 09:07:07
         .LINK
-            https://code.google.com/p/mod-posh/wiki/Untitled20#Start-WindowsUpdateDownload
+            https://code.google.com/p/mod-posh/wiki/WindowsUpdateLibrary#Start-WindowsUpdateDownload
     #>
     [CmdletBinding()]
     Param
@@ -80,16 +120,30 @@ Function Accept-WindowsUpdateEULA
 {
     <#
         .SYNOPSIS
+            Accept the EULA for updates
         .DESCRIPTION
+            This function accepts an UpdateCollection and allows you to accept the EULA 
+            on each update passed in.
+
+            This function leverages the Microsoft.Update comobjects in order to provide
+            an easy way for administrators to script update management.
         .PARAMETER Update
+            This is a collection of updates as returned from an update server.
         .PARAMETER AcceptEULA
+            A switch that if present will accept the EULA
         .EXAMPLE
+            Get-WindowsUpdate |Accept-WindowsUpdateEULA -AcceptEULA $true
+
+            Description
+            -----------
+            This example shows how to pass a collection of updates into the function on the
+            pipeline and accept the EULA for each update.
         .NOTES
             FunctionName : Accept-WindowsUpdateEULA
             Created by   : jspatton
             Date Coded   : 08/23/2013 09:27:47
         .LINK
-            https://code.google.com/p/mod-posh/wiki/Untitled20#Accept-WindowsUpdateEULA
+            https://code.google.com/p/mod-posh/wiki/WindowsUpdateLibrary#Accept-WindowsUpdateEULA
     #>
     [CmdletBinding()]
     Param
@@ -119,15 +173,27 @@ Function Install-WindowsUpdate
 {
     <#
         .SYNOPSIS
+            Install an update that has been downloaded
         .DESCRIPTION
+            This function will install a list of updates that have been downloaded to the 
+            local computer.
+
+            This function leverages the Microsoft.Update comobjects in order to provide
+            an easy way for administrators to script update management.
         .PARAMETER Update
+            This is a collection of updates as returned from an update server.
         .EXAMPLE
+            Get-WindowsUpdate |Install-WindowsUpdate
+
+            Description
+            -----------
+            This example shows passing a collection of updates, and then isntalling them.
         .NOTES
             FunctionName : Install-WindowsUpdate
             Created by   : jspatton
             Date Coded   : 08/23/2013 14:16:59
         .LINK
-            https://code.google.com/p/mod-posh/wiki/Untitled20#Install-WindowsUpdate
+            https://code.google.com/p/mod-posh/wiki/WindowsUpdateLibrary#Install-WindowsUpdate
     #>
     [CmdletBinding()]
     Param
@@ -167,9 +233,30 @@ Function Connect-WindowsUpdateServer
 {
     <#
         .SYNOPSIS
+            Connect to a local or remote Windows Update Server
         .DESCRIPTION
-        .PARAMETER
+            This function allows you to retreive information about updates on the local
+            or remote update server. This information is stored in a global variable
+            that can be accessed later as $Global:WSUSUpdateServer.
+
+            You will need the WSUS management bits installed in order for this cmdlet
+            to work properly.
+
+            This function leverages the Microsoft.UpdateServices comobjects in order to provide
+            an easy way for administrators to script update management.
+        .PARAMETER Server
+            If specified the function will attempt to connect to that server. The default port
+            of 8530 is used. If you use a different port, specify it on the command line.
+        .PARAMETER UseSecureConnection
+            If present will force the function to connect over ssl to the udpate server.
         .EXAMPLE
+            Connect-WindowsUpdateServer
+
+            Description
+            -----------
+            This example shows the basic syntax of the command.
+        .EXAMPLE
+            Connect-WindowsUpdateServer -Server updates.company.com:80
         .NOTES
             FunctionName : Connect-WindowsUpdateServer
             Created by   : jspatton
@@ -189,6 +276,7 @@ Function Connect-WindowsUpdateServer
         $Global:WSUSUpdateServer
         try
         {
+            $ErrorActionPreference = Stop
             $Assembly = [reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
 
             if ($Server)
@@ -214,7 +302,9 @@ Function Connect-WindowsUpdateServer
                 }
             else
             {
+                $ServerName = (& hostname)
                 $isLocal = $true
+                $PortNumber = 8530
                 }    
             }
         catch
