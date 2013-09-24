fd79d260f5389c085de0d060f772522899f0296a
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2013-09-24 13:58:18 +0000

    renamed some files
    git-svn-id: https://mod-posh.googlecode.com/svn@1005 b6353030-5bd9-47eb-cd4d-50672ec6839b

---
+++ C-Sharp/WindowsUpdateLibrary/WindowsUpdateLibrary/muaCmdlets.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+using System.Management.Automation;
+using System.Net;
+using Microsoft.UpdateServices.Administration;
+
+namespace WindowsUpdateLibrary
+{
+    class muaCmdlets
+    {
+        //
+        // Administrative Cmdlets
+        //
+        [Cmdlet(VerbsCommunications.Connect, "muaServer")]
+        public class Connect_muaServer : PSCmdlet
+        {
+            [Parameter(Mandatory = false,
+                HelpMessage = "Provide the name of your update server, if non-standard port servername:portnumber")]
+            [ValidateNotNullOrEmpty]
+            public string ServerName = Dns.GetHostName();
+
+            [Parameter(Mandatory = false,
+                HelpMessage = "If present use a secure connection to the server")]
+            [ValidateNotNullOrEmpty]
+            public bool UseSecureConnection = false;
+
+            bool isLocal = false;
+            int portNumber;
+            string wuaServer;
+            AdminProxy adminProxy = new AdminProxy();
+
+            protected override void BeginProcessing()
+            {
+                base.BeginProcessing();
+                try
+                {
+                    string localHost = Dns.GetHostName();
+                    if (ServerName.Contains(localHost))
+                    {
+                        isLocal = true;
+                    }
+
+                    if (ServerName.Contains(":"))
+                    {
+                        portNumber = Convert.ToInt16(ServerName.Substring(ServerName.IndexOf(":"), ServerName.Length - ServerName.IndexOf(":")).Replace(":", ""));
+                        wuaServer = ServerName.Substring(0, ServerName.IndexOf(":"));
+                    }
+                    else
+                    {
+                        portNumber = 8530;
+                        wuaServer = ServerName;
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            protected override void ProcessRecord()
+            {
+                base.ProcessRecord();
+                try
+                {
+                    if (isLocal)
+                    {
+                        Globals.updateServer = adminProxy.GetUpdateServerInstance();
+                    }
+                    else
+                    {
+                        Globals.updateServer = adminProxy.GetRemoteUpdateServerInstance(wuaServer, UseSecureConnection, portNumber);
+                    }
+                }
+                catch
+                {
+                }
+            }
+
+            protected override void EndProcessing()
+            {
+                base.EndProcessing();
+                WriteObject(Globals.updateServer);
+            }
+        }
+    }
+}
---
+++ C-Sharp/WindowsUpdateLibrary/WindowsUpdateLibrary/wuaCmdlets.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Management.Automation;
+using System.Net;
+using WUApiLib;
+using Microsoft.UpdateServices.Administration;
+
+namespace WindowsUpdateLibrary
+{
+    public class Globals
+    {
+        public static UpdateSession updateSession = new UpdateSession();
+        public static IUpdateSearcher updateSearcher = updateSession.CreateUpdateSearcher();
+        public static IUpdateServer updateServer;
+    }
+
+    public class wuaCmdlets
+    {
+        //
+        // End-User Cmdlets
+        //
+        [Cmdlet(VerbsCommon.Get, "wuaUpdate")]
+        public class Get_wuaUpdate : PSCmdlet
+        {
+            [Parameter(Mandatory = false,
+                HelpMessage = "Please provide a valid search criteria")]
+            [ValidateNotNullOrEmpty]
+            public string Criteria = "IsInstalled=0 and Type='Software'";
+
+            [Parameter(Mandatory = false,
+                HelpMessage = "If present get updates from Microsoft Servers")]
+            public SwitchParameter FromMicrosoft;
+
+            public ISearchResult searchResult;
+
+            protected override void BeginProcessing()
+            {
+                base.BeginProcessing();
+            }
+
+            protected override void ProcessRecord()
+            {
+                base.ProcessRecord();
+                if (FromMicrosoft)
+                {
+                    int ssWindowsUpdate = 2;
+                    Globals.updateSearcher.ServerSelection = (ServerSelection)ssWindowsUpdate;
+                }
+                searchResult = Globals.updateSearcher.Search(Criteria);
+            }
+
+            protected override void EndProcessing()
+            {
+                base.EndProcessing();
+                if (MyInvocation.BoundParameters.ContainsKey("Verbose"))
+                {
+                    foreach (WUApiLib.IUpdate Update in searchResult.Updates)
+                    {
+                        WriteVerbose("FOUND : " + Update.Title);
+                    }
+                }
+                WriteObject(searchResult.Updates);
+            }
+        }
+
+        [Cmdlet(VerbsLifecycle.Start, "wuaDownload")]
+        public class Start_wuaDownload : PSCmdlet
+        {
+            [Parameter(Mandatory = true, ValueFromPipeline = true,
+                HelpMessage = "The update to download")]
+            public WUApiLib.UpdateCollection Update;
+
+            WUApiLib.UpdateCollection updatesToDownload = new WUApiLib.UpdateCollection();
+            UpdateDownloader updateDownloader;
+
+            protected override void BeginProcessing()
+            {
+                base.BeginProcessing();
+                updateDownloader = Globals.updateSession.CreateUpdateDownloader();                
+            }
+
+            protected override void ProcessRecord()
+            {
+                base.ProcessRecord();
+                foreach (WUApiLib.IUpdate thisUpdate in Update)
+                {
+                    if ((thisUpdate.EulaAccepted) && !(thisUpdate.InstallationBehavior.CanRequestUserInput))
+                    {
+                        if (!(thisUpdate.IsDownloaded))
+                        {
+                            WriteVerbose("DOWNLOAD : " + thisUpdate.Title);
+                            updatesToDownload.Add(thisUpdate);
+                            updateDownloader.Updates = updatesToDownload;
+                            updateDownloader.Download();
+                        }
+                    }
+                }
+            }
+
+            protected override void EndProcessing()
+            {
+                base.EndProcessing();
+                WriteObject(updatesToDownload);
+            }
+        }
+
+        [Cmdlet(VerbsLifecycle.Install, "wuaUpdate")]
+        public class Install_wuaUpdate : PSCmdlet
+        {
+            [Parameter(Mandatory = true, ValueFromPipeline = true,
+                HelpMessage = "The update to download")]
+            public WUApiLib.UpdateCollection Update;
+
+            WUApiLib.UpdateCollection updatesToInstall = new WUApiLib.UpdateCollection();
+            bool RebootRequired = false;
+
+            protected override void BeginProcessing()
+            {
+                base.BeginProcessing();
+            }
+
+            protected override void ProcessRecord()
+            {
+                base.ProcessRecord();
+                foreach (WUApiLib.IUpdate thisUpdate in Update)
+                {
+                    if (thisUpdate.IsDownloaded)
+                    {
+                        updatesToInstall.Add(thisUpdate);
+                        if (thisUpdate.InstallationBehavior.RebootBehavior > 0)
+                        {
+                            RebootRequired = true;
+                        }
+                        IUpdateInstaller updateInstaller = Globals.updateSession.CreateUpdateInstaller();
+                        updateInstaller.Updates = updatesToInstall;
+                        IInstallationResult installationResult = updateInstaller.Install();
+                        WriteVerbose(installationResult.HResult.ToString());
+                    }
+                }
+            }
+
+            protected override void EndProcessing()
+            {
+                base.EndProcessing();
+                if (RebootRequired)
+                {
+                    WriteObject(RebootRequired);
+                }
+            }
+        }
+
+        [Cmdlet(VerbsCommon.Get, "wuaUpdateHistory")]
+        public class Get_wuaUpdateHistory : PSCmdlet
+        {
+            [Parameter(Mandatory = false,
+                HelpMessage = "Please enter a number to start out at (default=0)")]
+            public int startIndex = 0;
+
+            [Parameter(Mandatory = false,
+                HelpMessage = "Enter the number of updates to return (default=10)")]
+            public int Count = 10;
+
+            public IUpdateHistoryEntryCollection updateHistory;
+
+            protected override void BeginProcessing()
+            {
+                base.BeginProcessing();
+                if (Count > Globals.updateSearcher.GetTotalHistoryCount())
+                {
+                    Count = Globals.updateSearcher.GetTotalHistoryCount();
+                }
+
+                if (startIndex < 0)
+                {
+                    startIndex = 0;
+                }
+            }
+
+            protected override void ProcessRecord()
+            {
+                base.ProcessRecord();
+                updateHistory = Globals.updateSearcher.QueryHistory(startIndex, Count);
+            }
+
+            protected override void EndProcessing()
+            {
+                base.EndProcessing();
+                foreach (IUpdateHistoryEntry historyEntry in updateHistory)
+                {
+                    WriteObject(historyEntry);
+                }
+            }
+        }
+
+        [Cmdlet(VerbsCommon.Set, "wuaEULA")]
+        public class Set_wuaEULA : PSCmdlet
+        {
+            [Parameter(Mandatory = true, ValueFromPipeline = true,
+                HelpMessage = "The update to download")]
+            public WUApiLib.UpdateCollection Update;
+
+            [Parameter(Mandatory = false,
+                HelpMessage = "True or false to approve or not approve an update (default=true)")]
+            public bool AcceptEula = true;
+
+            protected override void BeginProcessing()
+            {
+                base.BeginProcessing();
+            }
+
+            protected override void ProcessRecord()
+            {
+                base.ProcessRecord();
+                foreach (WUApiLib.IUpdate thisUpdate in Update)
+                {
+                    if (!(thisUpdate.EulaAccepted))
+                    {
+                        if (AcceptEula)
+                        {
+                            thisUpdate.AcceptEula();
+                        }
+                    }
+                }
+            }
+
+            protected override void EndProcessing()
+            {
+                base.EndProcessing();
+            }
+        }
+    }
+}
