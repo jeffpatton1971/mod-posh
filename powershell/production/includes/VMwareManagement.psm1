964f0317cd7717d3ebf9ee47dd21f7d86849bfdb
jspatton1971@gmail.com <jspatton1971@gmail.com@b6353030-5bd9-47eb-cd4d-50672ec6839b>
2013-10-09 15:15:55 +0000

    Added a new function to hot-add cpu/memory and renamed functions so as not to class with the vmware builtins.
    git-svn-id: https://mod-posh.googlecode.com/svn@1012 b6353030-5bd9-47eb-cd4d-50672ec6839b

--- powershell/production/includes/VMwareManagement.psm1
+++ powershell/production/includes/VMwareManagement.psm1
@@ -1,4 +1,4 @@
-Function Get-VMHostNetworks
+Function Get-vmHostNetworks
 {
     <#
         .SYNOPSIS
@@ -26,7 +26,7 @@ Function Get-VMHostNetworks
             This script requires the VMware vSphere PowerCLI to be downloaded and installed, please see
             the second link for the download.
         .LINK
-            https://code.google.com/p/mod-posh/wiki/VMWareManagement#Get-VMHostNetworks
+            https://code.google.com/p/mod-posh/wiki/VMWareManagement#Get-vmHostNetworks
         .LINK
             http://communities.vmware.com/community/vmtn/server/vsphere/automationtools/powercli
         .LINK
@@ -74,7 +74,7 @@ Function Get-VMHostNetworks
         Return $VMNetworks
         }
     }
-Function Find-EntityView
+Function Find-vmEntityView
 {
     <#
         .SYNOPSIS
@@ -90,7 +90,7 @@ Function Find-EntityView
             Created by   : jspatton
             Date Coded   : 08/31/2013 08:35:22
         .LINK
-            https://code.google.com/p/mod-posh/wiki/Untitled22#Find-EntityView
+            https://code.google.com/p/mod-posh/wiki/VMWareManagement#Find-vmEntityView
     #>
     [CmdletBinding()]
     Param
@@ -193,7 +193,7 @@ Function Find-EntityView
         return $viObjects
         }
     }
-Function Get-View
+Function Get-vmView
 {
     <#
         .SYNOPSIS
@@ -205,7 +205,7 @@ Function Get-View
             Created by   : jspatton
             Date Coded   : 08/31/2013 18:45:43
         .LINK
-            https://code.google.com/p/mod-posh/wiki/Untitled22#Get-View
+            https://code.google.com/p/mod-posh/wiki/VMWareManagement#Get-vmView
     #>
     [CmdletBinding()]
     Param
@@ -226,6 +226,145 @@ Function Get-View
     End
     {
         return $viObjects
+        }
+    }
+Function Set-vmHotAddFeature
+{
+    <#
+        .SYNOPSIS
+            Enable or Disable the Hot Add memory and CPU feature for a VM
+        .DESCRIPTION
+            This function allows you to either enable or disable hot add features
+            on a VM. You will need to run this function inside the vSphere
+            PowerCLI shell, or have the VMWare Snap-in loaded.
+        .PARAMETER Name
+            The name of the virtual machine
+        .PARAMETER Feature
+            Currently supported features are
+                mem.hotadd
+                vcpu.hotadd
+        .PARAMETER Enable
+            This is either $true or $false, the default is set to $true
+        .EXAMPLE
+            Set-vmHotAddFeature cm12-test vcpu.hotadd
+
+            Name                                                        MemoryHotAddEnabled
+            ----                                                        -------------------
+            cm12-test                                                                  True
+
+            Description
+            -----------
+            This is the basic usage of the command
+        .EXAMPLE
+            "cm12-test","gpo-testing" |Set-vmHotAddFeature -Feature mem.hotadd -Enable $false
+
+            Name                                                        MemoryHotAddEnabled
+            ----                                                        -------------------
+            cm12-test                                                                 False
+            gpo-testing                                                               False
+
+            Description
+            -----------
+            This example shows how to pass names of vm's across the pipeline to the function
+        .NOTES
+            FunctionName : Set-vmHotAddFeature
+            Created by   : jspatton
+            Date Coded   : 10/09/2013 09:24:54
+        .LINK
+            https://code.google.com/p/mod-posh/wiki/VMWareManagement#Set-vmHotAddFeatures
+        .LINK
+            http://www.vmware.com/support/developer/PowerCLI/PowerCLI41/html/Get-VM.html
+        .LINK
+            http://www.vmware.com/support/developer/PowerCLI/PowerCLI41/html/Get-View.html
+    #>
+    [CmdletBinding()]
+    Param
+        (
+        [Parameter(Mandatory=$false,ValueFromPipeline=$true,Position=1)]
+        [string]$Name,
+        [Parameter(Mandatory=$True,Position=2)]
+        [ValidateSet("mem.hotadd","vcpu.hotadd")]
+        [string]$Feature,
+        [Parameter(Position=3)]
+        [bool]$Enable = $True
+        )
+    Begin
+    {
+        try
+        {
+            $vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
+            $optionValue = New-Object VMware.Vim.OptionValue
+            }
+        catch
+        {
+            Write-Error $Error[0]
+            break
+            }
+        }
+    Process
+    {
+        try
+        {
+            Write-Verbose "Get the view object for the passed in VM"
+            $Vm = Get-VM -Name $Name
+            $vmView = $VM |Get-View
+            Write-Verbose "Changing $($Feature)"
+            switch ($Feature)
+            {
+                "mem.hotadd"
+                {
+                    if ($Enable)
+                    {
+                        Write-Verbose "Enable $($Feature)"
+                        $optionValue.Key = "mem.hotadd"
+                        $optionValue.Value = "true"
+                        }
+                    else
+                    {
+                        Write-Verbose "Disable $($Feature)"
+                        $optionValue.Key = "mem.hotadd"
+                        $optionValue.Value = "false"
+                        }
+                    }
+                "vcpu.hotadd"
+                {
+                    if ($Enable)
+                    {
+                        Write-Verbose "Enable $($Feature)"
+                        $optionValue.Key = "vcpu.hotadd"
+                        $optionValue.Value = "true"
+                        }
+                    else
+                    {
+                        Write-Verbose "Disable $($Feature)"
+                        $optionValue.Key = "vcpu.hotadd"
+                        $optionValue.Value = "false"
+                        }
+                    }
+                }
+            Write-Verbose "Update VM"
+            $vmConfigSpec.ExtraConfig += $optionValue
+            $vmView.ReconfigVM($vmConfigSpec)
+            switch ($Feature)
+            {
+                "mem.hotadd"
+                {
+                    Return $VM |Get-View |Select-Object Name, @{Name="MemoryHotAddEnabled";Expression={$_.Config.MemoryHotAddEnabled}}
+                    }
+                "vcpu.hotadd"
+                {
+                    Return $VM |Get-View |Select-Object Name, @{Name="CpuHotAddEnabled";Expression={$_.Config.CpuHotAddEnabled}}
+                    }
+                }
+            }
+        catch
+        {
+            Write-Error $Error[0]
+            break
+            }
+        }
+    End
+    {
         }
     }
 Export-ModuleMember *
