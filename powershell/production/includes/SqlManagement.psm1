5864e9b85f68a33ba0094c6f2a474c3c52deb2d2
Jeff Patton <jspatton@ku.edu>
2014-06-03 12:38:58 -0500

    Added a function to get a list of databases

--- powershell/production/includes/SqlManagement.psm1
+++ powershell/production/includes/SqlManagement.psm1
@@ -321,3 +321,64 @@ function Get-SqlUser
             }
         }
     }
+function Get-SqlDatabase
+{
+    param 
+    (
+        [parameter(Mandatory = $true)][string] $sqlServer,
+        [parameter(Mandatory = $true)][string] $Database,
+        [parameter(Mandatory = $false)][string] $sqlInstance,
+        [parameter(Mandatory = $false)][PSCredential] $Credential
+    )
+
+    $sqlConnection = $null
+
+    try
+    {
+        $Error.Clear()
+        if ($sqlInstance -eq $null)
+        {
+            $sqlConnString = "Server=tcp:$($sqlServer);Database=$($Database)";
+            }
+        else
+        {
+            $sqlConnString = "Server=tcp:$($sqlServer)\$($sqlInstance);Database=$($Database)";
+            }
+        if ($Credential)
+        {
+            $Credential.Password.MakeReadOnly();
+            $sqlCredential = New-Object System.Data.SqlClient.SqlCredential($Credential.UserName, $Credential.Password);
+            }
+        else
+        {
+            $sqlConnString += ";trusted_connection=true";
+            }
+        Write-Verbose $sqlConnString
+        $sqlConnBuilder = New-Object System.Data.SqlClient.SqlConnectionStringBuilder($sqlConnString);
+        Write-Verbose $sqlConnBuilder.ConnectionString
+        $sqlConnection = New-Object System.Data.SqlClient.SqlConnection($sqlConnBuilder.ConnectionString);
+        $sqlCommandText = "SELECT name FROM master..sysdatabases"
+        Write-Verbose $sqlCommandText
+        if ($sqlCredential)
+        {
+            $sqlConnection.Credential = $sqlCredential;
+            }
+        $sqlConnection.Open()
+        $sqlDataSet = New-Object System.Data.DataSet
+        $sqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter ($sqlCommandText, $sqlConnection)
+        $Result = $sqlAdapter.Fill($sqlDataSet)
+        $sqlDataSet.Tables;
+        }
+    catch
+    {
+        $str = (([string] $Error).Split(':'))[1]
+        Write-Error ($str.Replace('"', ''))
+        }
+    finally
+    {
+        if ($sqlConnection)
+        {
+            $sqlConnection.Close()
+            }
+        }
+    }
