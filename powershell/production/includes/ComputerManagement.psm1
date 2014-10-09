c55f409b930a19068d6b8e45664b11aa632060de
Jeff Patton <jeffrey@patton-tech.com>
2014-10-09 13:37:33 -0500

    A function to tail windows event logs

--- powershell/production/includes/ComputerManagement.psm1
+++ powershell/production/includes/ComputerManagement.psm1
@@ -2268,4 +2268,76 @@ Function Get-NetShare
     {
         }
     }
+Function Get-WinEventTail
+{
+    <#
+        .SYNOPSIS
+            A tail cmdlet for Eventlogs
+        .DESCRIPTION
+            This function will allow you to tail Windows Event Logs. You specify
+            a Logname for either the original logs, Application, System and Security or 
+            the new format for the newer logs Microsoft-Windows-PowerShell/Operational
+        .PARAMETER LogName
+            Specify a valid Windows Eventlog name
+        .PARAMETER ShowExisting
+            An integer to show the number of events to start with, the default is 10
+        .EXAMPLE
+            Get-WinEventTail -LogName Application
+
+
+               ProviderName: ESENT
+
+            TimeCreated                     Id LevelDisplayName Message                                                                                                                                                                                                      
+            -----------                     -- ---------------- -------                                                                                                                                                                                                      
+            10/9/2014 11:55:51 AM          102 Information      svchost (7528) Instance: ...
+            10/9/2014 11:55:51 AM          105 Information      svchost (7528) Instance: ...
+            10/9/2014 11:55:51 AM          326 Information      svchost (7528) Instance: ...
+            10/9/2014 12:05:49 PM          327 Information      svchost (7528) Instance: ...
+            10/9/2014 12:05:49 PM          103 Information      svchost (7528) Instance: ...
+
+        .NOTES
+            FunctionName : Get-WinEventTail
+            Created by   : jspatton
+            Date Coded   : 10/09/2014 13:20:22
+        .LINK
+            https://code.google.com/p/mod-posh/wiki/ComputerManagement#Get-WinEventTail
+        .LINK
+            http://stackoverflow.com/questions/15262196/powershell-tail-windows-event-log-is-it-possible
+    #>
+    [CmdletBinding()]
+    Param
+        (
+        [string]$LogName = 'Application',
+        [int]$ShowExisting = 10
+        )
+    Begin
+    {
+        if ($ShowExisting -gt 0)
+        {
+            $Data = Get-WinEvent -LogName $LogName -MaxEvents $ShowExisting
+            $Data | Sort-Object RecordId
+            $Index1 = $Data[0].RecordId
+            }
+        else
+        {
+            $Index1 = (Get-WinEvent -LogName $LogName -max 1).RecordId
+            }
+        }
+    Process
+    {
+        while ($true)
+        {
+            start-sleep -Seconds 1
+            $Index2  = (Get-WinEvent -LogName $LogName -max 1).RecordId
+            if ($Index2 -gt $Index1)
+            {
+                Get-WinEvent -LogName $LogName -max ($Index2 - $Index1) | Sort-Object RecordId
+                }
+            $Index1 = $Index2
+            }
+        }
+    End
+    {
+        }
+    }
 Export-ModuleMember *
